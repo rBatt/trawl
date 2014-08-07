42b90c9218afb717a6ee1726af9a09fefeed0892
rbatt <battrd@gmail.com>
2014-08-07 17:52:01 -0400

    updated raw data, began creating scripts for reading data

---
+++ Scripts/read.aleutians.R
@@ -0,0 +1,7 @@
+
+# ====================
+# = Read in Raw Data =
+# ====================
+ai1.raw <- read.table("/Users/Battrd/Documents/School&Work/pinskyPost/trawl/Data/raw_data/AFSC_Aleutians/2013-10-17/ai1983_2000.csv", header=TRUE, sep=",")
+ai2.raw <- read.table("/Users/Battrd/Documents/School&Work/pinskyPost/trawl/Data/raw_data/AFSC_Aleutians/2013-10-17/ai2002_2012.csv", header=TRUE, sep=",", quote="")
+aiStrata.raw <- read.table("/Users/Battrd/Documents/School&Work/pinskyPost/trawl/Data/raw_data/AFSC_Aleutians/2013-10-17/aiStrata.csv", header=TRUE, sep=",")
