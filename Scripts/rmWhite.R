c54bdbf0190e97dc31bdac670335a12bfffec44b
rbatt <battrd@gmail.com>
2014-08-18 09:52:36 -0400

    updated script names, adjusted rmWhite
    Former-commit-id: 2164583a69853cbf72f4dd0b920bdf8fbac774ef

--- Scripts/rmWhite.R
+++ Scripts/rmWhite.R
@@ -1,9 +1,10 @@
 
 rmWhite <- function(x){
 	stopifnot(is.data.table(x))
+	nmx <- names(x)
 	for(i in 1:ncol(x)){
-		t.name <- names(x)[i]
-		expr <- bquote(.(as.name(t.name)):=gsub("^\\s* | \\s*$", "", .(as.name(t.name))))
+		t.name <- as.name(nmx[i])
+		expr <- bquote(.(t.name):=gsub("^\\s* | \\s*$", "", .(t.name)))
 		x[,eval(expr)]
 	}
 }
