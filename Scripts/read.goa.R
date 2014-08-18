58c34c8eab8993eb3ac43e96e5e374e732f7baea
rbatt <battrd@gmail.com>
2014-08-18 17:29:58 -0400

    finished raw read, some qa/qc
    Former-commit-id: a2c055826e95773dce35ae3871316cd046d5fb3f

--- Scripts/read.goa.R
+++ Scripts/read.goa.R
@@ -1,26 +1,9 @@
 
 # NOTE: using the data.table code took 0.790 seconds, whereas the original code took 5.567 seconds
 
-# ================
-# = Read in Data =
-# ================
-# goaStrata.raw <- read.table("/Users/Battrd/Documents/School&Work/pinskyPost/trawl/Data/raw_data/AFSC_GOA/2013-10-17/goaStrata.csv", header=TRUE, sep=",")
-# 
-# goa.files <- list.files("/Users/Battrd/Documents/School&Work/pinskyPost/trawl/Data/raw_data/AFSC_GOA/2013-10-17/")
-# goa.files <- goa.files[!goa.files%in%"goaStrata.csv"]
-# n.goa <- length(goa.files)
-# 
-# for(i in 1:n.goa){ # loop through data files and combine them. Assumes that column headers match
-# 	t.goa.file <- goa.files[i]
-# 	t.goa.name <- paste("/Users/Battrd/Documents/School&Work/pinskyPost/trawl/Data/raw_data/AFSC_GOA/2013-10-17/", t.goa.file, sep="")
-# 	if(i==1){
-# 		goa.raw <- read.table(t.goa.name, header=TRUE, sep=",", quote="")
-# 	}else{
-# 		goa.raw <- rbind(goa.raw, read.table(t.goa.name, header=TRUE, sep=",", quote=""))
-# 	}
-# 	
-# }
-
+library(data.table)
+source("/Users/Battrd/Documents/School&Work/pinskyPost/trawl/Scripts/rmWhite.R")
+source("/Users/Battrd/Documents/School&Work/pinskyPost/trawl/Scripts/rm9s.R")
 
 
 goaStrata.raw <- fread("/Users/Battrd/Documents/School&Work/pinskyPost/trawl/Data/raw_data/AFSC_GOA/2013-10-17/goaStrata.csv", header=TRUE, sep=",")
