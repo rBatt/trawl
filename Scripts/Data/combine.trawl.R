6c1ee358c730c1110047c74747ace59a33dfc5ba
rbatt <battrd@gmail.com>
2014-10-03 13:08:03 -0400

    clean up comments

--- Scripts/Data/combine.trawl.R
+++ Scripts/Data/combine.trawl.R
@@ -18,6 +18,7 @@ cleanedRegions <- list.files(cleanDirectory)
 cleanNames.reg <- regexpr("[a-z]+[0-9]?(?=\\.RData$)", cleanedRegions, perl=TRUE)
 cleanNames <- regmatches(cleanedRegions, cleanNames.reg)
 
+
 # ==========================================
 # = Load regions locally, combine globally =
 # ==========================================
@@ -30,11 +31,11 @@ local({
 	rm(list=ls()) # thus removing ls() preserves trawl and all variables defined outside of call to local()
 })
 
+
 # ============================
 # = Convert spp to character =
 # ============================
 trawl000[,spp:=as.character(spp)]
-
 
 
 # ======================
@@ -52,21 +53,10 @@ badSpp <- unique(c(badEgg, badFish, badLarv, badYoy))
 setkey(trawl000, spp)
 trawl00 <- trawl000[!.(badSpp),]
 
-# ========================
-# = Note redundant names =
-# ========================
-# "ACANTHOCARPUS ALEXANDRI"
-# # and
-# "ACATHOCARPUS ALEXADRI" 
-
-# trawl["ACATHOCARPUS ALEXADRI", spp:="ACANTHOCARPUS ALEXANDRI"] # fix a redundant species name due to typo
-
 
 # ====================================
 # = Functions for cleaning spp names =
 # ====================================
-
-
 fixCase <- function(x){
 	s <- paste(toupper(substring(x, 1, 1)), substring(x, 2), sep="")
 	paste(substring(s, 1, 1), tolower(substring(s, 2)), sep="")
@@ -77,7 +67,6 @@ cullExSpace <- function(x){
 }
 
 cullSp <- function(x){
-	# gsub("\\ss[p]{1,2}\\..*", "", x)
 	gsub("\\s(s[p]{1,2}|unid)\\..*", "", x)
 }
 
@@ -88,6 +77,7 @@ cullParen <- function(x){
 is.species <- function(x){
 	sapply(strsplit(x, " "), length) >= 2
 }
+
 
 # =============================
 # = Clean and reaggregate spp =
@@ -106,13 +96,7 @@ setkey(raw.and.spp, raw.spp, spp)
 raw.and.spp <- unique(raw.and.spp)
 save(raw.and.spp, file="/Users/Battrd/Documents/School&Work/pinskyPost/trawl/Data/Taxonomy/raw.and.spp.RData")
 
-
 trawl00[,isSpecies:=is.species(spp)] # infer whether the taxa are identified to species or to genus (1 or 2 words)
-
-# trawl2 <- trawl
-# trawl2[,c("wtcpue", "cntcpue","depth","btemp","stemp"):=list(wtcpue=sumna(wtcpue), cntcpue=sumna(cntcpue), depth=meanna(depth), btemp=meanna(btemp), stemp=meanna(stemp)), by=c("year","datetime","spp","haulid","stratum","stratumarea","lat","lon","region","s.reg")]
-# dim(trawl) # 997913     15
-# dim(trawl2) # 997913     15 ... this means that species weren't ID'd in multiple ways during the same haul.
 
 
 # ================================
@@ -162,8 +146,6 @@ tax.files <- dir("/Users/Battrd/Documents/School&Work/pinskyPost/trawl/Data/Taxo
 if(!"spp.corr1.RData"%in%tax.files){
 	print("Can't find data file, looking up all spp names")
 	flush.console()
-	# cut(seq_along(uspp), length(uspp)/(2:20)[which.min(length(uspp)%%(2:20))])
-	# n.cuts <- length(uspp)/(2:20)[which.min(length(uspp)%%(2:20))] # finds the smallest integer between 2 and 20 of which length(uspp) is a multiple (modulus %% == 0)
 	uspp.chunks <- as.character(cut(seq_along(uspp), length(uspp)))
 	u.uspp.chunks <- unique(uspp.chunks)
 	# start.time <- proc.time()["elapsed"]/60/60
@@ -180,17 +162,7 @@ if(!"spp.corr1.RData"%in%tax.files){
 			spp.corr1 <- rbind(spp.corr1, t.spp.corr1)	
 		}
 		setTxtProgressBar(spp.pb, s)
-		# t.time <- proc.time()["elapsed"]/60/60
-		# t.perc <- formatC(round(s/length(u.uspp.chunks)*100,2), 3)
-		# t.elap <- formatC(round(t.time-start.time, 2),3)
-		# print(paste("# ", formatC(s, digits=4), " has completed (", t.perc, "%); ", t.elap, " hours elapsed", sep=""))
-		# flush.console()
 	}
-	
-	# spp.corr1.names <- gnr_resolve(uspp, stripauthority=TRUE, http="post")
-	# save(spp.corr1.names, file="/Users/Battrd/Documents/School&Work/pinskyPost/trawl/Data/Taxonomy/spp.corr1.names.RData")
-	# spp.corr1 <- ddply(spp.corr1.names, "submitted_name", grb.spp1)
-	# spp.corr1 <- data.table(spp.corr1)
 	close(spp.pb)
 	setnames(spp.corr1, c("submitted_name", "matched_name2"), c("spp", "sppCorr"))
 	setkey(spp.corr1, spp)
@@ -212,7 +184,6 @@ if(!"spp.corr1.RData"%in%tax.files){
 		new.spp.chunks <- as.character(cut(seq_along(new.spp), length(new.spp))) # could just use unique(), but this is here if we want to search for more than 1 new.spp at a time (i.e., several new.spp would be passed to gnr_resolve at once)
 		u.new.spp.chunks <- unique(new.spp.chunks) # the unique groups of species that will be saerched
 		
-		# start.time <- proc.time()["elapsed"]/60/60 # set up timer
 		spp.pb <- txtProgressBar(min=1, max=length(u.new.spp.chunks), style=3)
 		for(s in seq_along(u.new.spp.chunks)){ # for each group of new species to search (current just each unique species)
 		
@@ -226,11 +197,6 @@ if(!"spp.corr1.RData"%in%tax.files){
 				spp.corr2 <- rbind(spp.corr2, t.spp.corr2)	
 			}
 			setTxtProgressBar(spp.pb, s)
-			# t.time <- proc.time()["elapsed"]/60/60
-# 			t.perc <- formatC(round(s/length(u.new.spp.chunks)*100,2), 3)
-# 			t.elap <- formatC(round(t.time-start.time, 2),3)
-# 			print(paste("# ", formatC(s, digits=4), " has completed (", t.perc, "%); ", t.elap, " hours elapsed", sep=""))
-# 			flush.console()
 			
 		}
 		close(spp.pb)
@@ -258,13 +224,6 @@ if(!"spp.corr1.RData"%in%tax.files){
 	}
 }
 
-
-
-
-
-# setkey(spp.corr1, spp, sppCorr)
-# dim(unique(spp.corr1))
-# spp.corr1 <- unique(spp.corr1)
 
 # ===========================
 # = Search for common names =
@@ -359,12 +318,7 @@ if(!"spp.cmmn1.RData"%in%tax.files){
 		close(cmmn.pb) # close progress bar
 		setkey(spp.cmmn2, sppCorr) # set key for new common names
 		spp.cmmn1 <- rbind(spp.cmmn1, spp.cmmn2) # bind new and old common names
-		
-			# ============================
-			# = Some mannual corrections =
-			# ============================
-			
-		
+
 		save(spp.cmmn1, file="/Users/Battrd/Documents/School&Work/pinskyPost/trawl/Data/Taxonomy/spp.cmmn1.RData")
 	}
 }
@@ -381,29 +335,14 @@ trawl.newSpp[!grepl("[a-zA-Z]", common)|common=="", common:=as.character(NA)] # 
 
 # check for duplicates (arising because of different original "spp" value, but resolved to be same sppCorr & common values)
 setkey(trawl.newSpp, sppCorr, common)
-# sum(duplicated(trawl.newSpp))
 trawl.newSpp <- unique(trawl.newSpp)
+
 
 # =============================
 # = Determine taxonomic level =
 # =============================
-
-# t.taxLvl0 <- tryCatch( # first try finding the common name in itis
-# 	{
-# 		tail(classification("Abisa", db="itis", verbose=FALSE)[[1]][,2], 1)
-# 	},
-# 		error=function(cond){as.character(NA)}
-# )
-# "\\n"
-# tail(classification("Cetacea", db="itis")[[1]][,2], 1)
-# classification("Abisa", db="itis")
 sppCorr2 <- trawl.newSpp[,sppCorr]
 class.names <- c("species", "genus", "family", "order", "class", "superclass", "subphylum", "phylum", "kingdom")
-
-# formals(get_tsn)$ask <- FALSE
-
-
-
 
 if("taxLvl1.RData"%in%tax.files){
 	print("File of common names found")
@@ -468,96 +407,7 @@ if("taxLvl1.RData"%in%tax.files){
 	save(taxLvl1, file="/Users/Battrd/Documents/School&Work/pinskyPost/trawl/Data/Taxonomy/taxLvl1.RData")
 	
 }
-	
-	#
-#
-# 	print("File of taxonomic levels not found, searching for all")
-# 	flush.console()
-# 	tlvl.pb <- txtProgressBar(min=1, max=length(sppCorr2), style=3)
-# 	for(i in 1:length(sppCorr2)){
-# 		t.classification <- tryCatch( # first try finding the common name in itis
-# 			{
-# 				classification(get_tsn(sppCorr2[i], ask=FALSE, verbose=FALSE), verbose=FALSE)[[1]]
-# 			},
-# 				error=function(cond){as.character(NA)}
-# 		)
-# 		# t.taxLvl0 <- t.taxLvl00[grepl("[a-zA-Z]", t.taxLvl00)][1] # only match common names with english chars
-#
-# 		# t.class <- as.data.frame(matrix(NA, ncol=length(class.names), dimnames=list(NULL,class.names)))
-# 		#
-# 		# t.class
-#
-# 		if(!is.na(t.classification)){
-# 			t.taxLvl0 <- tail(t.classification[,2], 1) # 2nd column contains level of classification, tail(,1) to get most specific
-#
-# 			t.c1 <- tolower(t.classification[,2]) # column 1 of classification, in lower case
-# 			t.c2 <- t.classification[,1] # column 2 of classification, which is class. level
-# 			t.c1.ind <- t.c1%in%class.names # index of which levels of classification should be extracted
-#
-# 			t.taxLvl1 <- data.table(sppCorr=sppCorr2[i], taxLvl=t.taxLvl0) # create a data table
-# 			t.taxLvl1[,(class.names):=NA] # create empty columns for classification
-# 			t.taxLvl1[,t.c1[t.c1.ind]:=as.list(t.classification[t.c1.ind,1])] # fill in empty classification columns where found
-#
-#
-# 		}else{ # else, if the call to classification() or get_tsn() failed, then
-# 			t.taxLvl0 <- NA # leave taxLvl as NA
-# 			t.taxLvl1 <- data.table(sppCorr=sppCorr2[i], taxLvl=t.taxLvl0) # record corrected spp name, and NA tax lvl
-# 			t.taxLvl1[,(class.names):=NA] # and the classification will be left as NA
-# 		}
-#
-#
-# 		# t.taxLvl1 <- data.table(sppCorr=sppCorr2[i], taxLvl=t.taxLvl0) # turn the common match into a data table w/ sppCorr
-# 		if(i==1){
-# 			taxLvl1 <- t.taxLvl1 # create the spp.cmmn1 data.table
-# 		}else{
-# 			taxLvl1 <- rbind(taxLvl1, t.taxLvl1) # or accumulate the spp.cmmn1 entries
-# 		}
-# 		setTxtProgressBar(tlvl.pb, i)
-# 	}
-# 	close(tlvl.pb)
-# 	setkey(taxLvl1, sppCorr)
-# 	save(taxLvl1, file="/Users/Battrd/Documents/School&Work/pinskyPost/trawl/Data/Taxonomy/taxLvl1.RData")
-#
-# 	# ==========================================================
-# 	# = If taxLvl.RData does exist, only search for new common =
-# 	# ==========================================================
-# }else{
-# 	print("File of common names found")
-# 	flush.console()
-# 	load("/Users/Battrd/Documents/School&Work/pinskyPost/trawl/Data/Taxonomy/taxLvl1.RData")
-# 	setkey(taxLvl1, sppCorr)
-# 	new.sppCorr0 <- !u.sppCorr%in%taxLvl1[,sppCorr] & !is.na(u.sppCorr)
-# 	if(any(new.sppCorr0)){
-# 		print(paste("Looking up taxonomic level for ", sum(new.sppCorr0), " new spp", sep=""))
-# 		flush.console()
-# 		new.sppCorr <- u.sppCorr[new.sppCorr0]
-# 		tlvl.pb <- txtProgressBar(min=1, max=length(new.sppCorr), style=3) # initialize the progress bar
-# 		for(i in 1:length(new.sppCorr)){
-# 			t.taxLvl0 <- tryCatch( # first try finding the common name in itis
-# 				{
-# 					# tail(classification(sppCorr2[i], db="itis", verbose=FALSE)[[1]][,2], 1)
-# 					tail(classification(get_tsn(new.sppCorr[i], ask=FALSE, verbose=FALSE), verbose=FALSE)[[1]][,2], 1)
-# 				},
-# 					error=function(cond){as.character(NA)}
-# 			)
-# 			t.taxLvl2 <- data.table(sppCorr=new.sppCorr[i], taxLvl=t.taxLvl0)
-# 			if(i==1){
-# 				taxLvl2 <- t.taxLvl2
-# 			}else{
-# 				taxLvl2 <- rbind(taxLvl2, t.taxLvl2)
-# 			}
-#
-# 			setTxtProgressBar(tlvl.pb, i) # update progress bar
-# 		}
-# 		close(tlvl.pb) # close progress bar
-# 		setkey(taxLvl2, sppCorr) # set key for new common names
-# 		taxLvl1 <- rbind(taxLvl1, taxLvl2) # bind new and old common names
-# 		save(taxLvl1, file="/Users/Battrd/Documents/School&Work/pinskyPost/trawl/Data/Taxonomy/taxLvl1.RData")
-# 	}
-# }
 
-
-# taxLvl1[,sum(!is.na(taxLvl)&(taxLvl%in%c("Species")|(grepl("\\s", sppCorr)&taxLvl=="Genus")))]
 
 # ======================================
 # = Update species names in trawl data =
