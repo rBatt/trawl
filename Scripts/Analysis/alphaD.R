537e0502ddf5c08aa7da08d94d5997a32d5c88f0
rbatt <battrd@gmail.com>
2014-10-16 08:48:46 -0400

    Correct alpha D trend, and alpha now
    Former-commit-id: 3e80af996fd6478bf77b9c169e7a221c1c58707a

--- Scripts/Analysis/alphaD.R
+++ Scripts/Analysis/alphaD.R
@@ -1,49 +1,141 @@
 
+# ==================
+# = Load Libraries =
+# ==================
+# library(maps)
+library(data.table)
+library(vegan)
+library(reshape2)
+
+
+# =============
+# = Load Data =
+# =============
+# Load trawl data
+load("/Users/Battrd/Documents/School&Work/pinskyPost/trawl/Data/divData.RData")
+
+
+# ===========================
+# = Load Scripts/ Functions =
+# ===========================
+# Load Data functions
+dat.location <- "~/Documents/School&Work/pinskyPost/trawl/Scripts/DataFunctions"
+invisible(sapply(paste(dat.location, list.files(dat.location), sep="/"), source, .GlobalEnv))
+lu <- function(x) length(unique(x))
+
+# Load plottign functions
+plot.location <- "~/Documents/School&Work/pinskyPost/trawl/Scripts/PlotFunctions"
+invisible(sapply(paste(plot.location, list.files(plot.location), sep="/"), source, .GlobalEnv))
+
+# Load statistics functions
+plot.location <- "~/Documents/School&Work/pinskyPost/trawl/Scripts/StatFunctions"
+invisible(sapply(paste(plot.location, list.files(plot.location), sep="/"), source, .GlobalEnv))
+
+# divData2 <- divData[,list(stemp=meanna(stemp), btemp=meanna(btemp), depth=meanna(depth), wtcpue=meanna(wtcpue)), by=c("s.reg","stratum","spp","year","common")]
+
 
 
 
 # ===================
 # = Alpha over time =
 # ===================
-
-alpha.turn.time.expr <- bquote({
-	# print(paste(s.reg, stratum))
-	# print(.SD)
+alpha.trend.expr <- bquote({
 	if(lu(year)>3){
 		castExp <- acast(melt(.SD, id.vars=c("year","spp"), measure.vars=c("wtcpue")), year~spp)[,-1]
-		d.helli <- diversity(castExp)
+		alphaD <- diversity(castExp)
 		
-		print(d.helli)
+		yrs.0 <- as.numeric(names(alphaD))
+		dX.yr <- yrs.0 - min(yrs.0)
 
-		
-		dX.yr <- 1:length(d.helli)
-		
-		good.y1 <- d.helli>0 # figure out which indices would throw error if took log
-		dy1 <- d.helli[good.y1] #log(d.helli[good.y1])
-		dX <- c(dX.yr)[good.y1]
-		# plot(dX,dy1, type="o")
+		dy1 <- as.numeric(alphaD)
+		dX <- c(dX.yr)
+			
 		decay.slope <- lm(dy1~dX)$coef[2]
 		decay.slope
+		
+	}else{
+		as.numeric(NA)
+	}
+})
+
+
+
+# =================
+# = Current Alpha =
+# =================
+alpha.now.expr <- bquote({
+	if(lu(year)>3){
+		castExp <- acast(melt(.SD, id.vars=c("year","spp"), measure.vars=c("wtcpue")), year~spp)[,-1]
+		alphaD <- diversity(castExp)
+
+		yrs.0 <- as.numeric(names(alphaD))
+		dX.yr <- yrs.0 - min(yrs.0)
+
+		dy1 <- as.numeric(alphaD)
+		dX <- c(dX.yr)
+		
+		now.pred <- rev(as.numeric(predict(lm(dy1~dX))))[1]
+		if(now.pred<=0){
+			rev(dy1)[1]
 		}else{
-			as.numeric(NA)
+			now.pred
 		}
+		
+	}else{
+		as.numeric(NA)
+	}
+
 
 })
 
 
 
-# alpha.turn.time <- trawl3[,list(lon=mean(lon), lat=mean(lat), turn.time=eval(alpha.turn.time.expr)), by=c("s.reg","stratum")]
-alpha.turn.time <- divData[,
+# ========================
+# = Plot Alpha over time =
+# ========================
+alpha.plot.expr <- bquote({
+	if(lu(year)>3){
+		castExp <- acast(melt(.SD, id.vars=c("year","spp"), measure.vars=c("wtcpue")), year~spp)[,-1]
+		alphaD <- diversity(castExp)
+		
+		yrs.0 <- as.numeric(names(alphaD))
+		yrs.1 <- min(yrs.0)
+		dX.yr <- yrs.0 - yrs.1
+
+		dy1 <- as.numeric(alphaD)
+		dX <- c(dX.yr)
+		
+			
+		alpha.mod <- lm(dy1~yrs.0)
+		am.ylim <- range(c(predict(alpha.mod), dy1), na.rm=TRUE)
+		
+		par(mar=c(2.5, 2.5, 0.1, 0.5), ps=8, cex=1, mgp=c(1.5, 0.25, 0), tcl=-0.25, family="Times")
+		plot(yrs.0, dy1, pch=20, ylim=am.ylim)
+		abline(alpha.mod, col="red")
+		points(tail(yrs.0,1), tail(predict(alpha.mod),1), pch=19, col="blue")
+		print(tail(predict(alpha.mod),1))
+			
+		}else{
+			as.numeric(NA)
+		}
+})
+
+# dev.new()
+# divData[s.reg=="ai"&stratum=="513",
+# 	j={
+# 		eval(alpha.plot.expr)
+# 	}
+# ]
+
+
+
+alphaD <- divData[,
 	j={
-		list(lon=mean(lon), lat=mean(lat), turn.time=eval(alpha.turn.time.expr))
+		list(lon=mean(lon), lat=mean(lat), alpha.trend=eval(alpha.trend.expr), alpha.now=eval(alpha.now.expr))
 	}, 
 	
 	by=c("s.reg","stratum")
 ]
-alpha.turn.time <- alpha.turn.time[!is.na(turn.time)&turn.time>0,]
-alpha.turn.time[,turn.time:=log(turn.time)]
-
-setkey(alpha.turn.time, s.reg, stratum)
 
 
 
