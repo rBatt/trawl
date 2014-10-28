36b132d6dd7166dcd6d0ea39b9d39cc88334ef8e
rbatt <battrd@gmail.com>
2014-10-28 13:24:20 -0400

    Remove ignored files, fwf and zip
    Former-commit-id: d8adccb93ad3221ca2b65c939e235a0832d3fd93

--- Scripts/DataFunctions/calcarea.R
+++
@@ -1,9 +0,0 @@
-calcarea <- function(lonlat){
-	hullpts = chull(x=lonlat[,1], y=lonlat[,2]) # find indices of vertices
-	hullpts = c(hullpts,hullpts[1]) # close the loop
-	ps = appendPolys(NULL,mat=as.matrix(lonlat[hullpts,]),1,1,FALSE) # create a Polyset object
-	attr(ps,"projection") = "LL" # set projection to lat/lon
-	psUTM = convUL(ps, km=TRUE) # convert to UTM in km
-	polygonArea = calcArea(psUTM,rollup=1)
-	return(polygonArea$area)
-}
--- Scripts/DataFunctions/sumna.R
+++
@@ -1,4 +0,0 @@
-sumna <- function(x){
-	if(!all(is.na(x))) return(sum(x, na.rm=T))
-	if(all(is.na(x))) return(as.numeric(NA))
-}
