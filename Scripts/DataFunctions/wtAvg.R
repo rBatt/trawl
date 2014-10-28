634c8a6bc2b3f684b851c87f12d7d50a3d4e5deb
rbatt <battrd@gmail.com>
2014-10-28 13:12:59 -0400

    Remove ignored files

--- Scripts/DataFunctions/wtAvg.R
+++
@@ -1,8 +0,0 @@
-# Avg by wtcpue
-wtAvg <- function(x,y){
-	# x is something like temperature (the value to be averaged)
-	# y is something like wtcpue (the value to be used for weighting)
-	totW <- sum(y[is.finite(x)])
-	propW <- y/totW
-	sumna(x*propW)
-}
