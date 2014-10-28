3888bd1928ec9c2d4b6097b7d342736f3c6a26af
rbatt <battrd@gmail.com>
2014-10-28 17:42:43 -0400

    time trend, spat grad ip
    Have calculated the temporal temperature trend for each grid cell, now I'm working on calculating the spatial gradient.

--- Scripts/Data/read.HadISST.R
+++ Scripts/Data/read.HadISST.R
@@ -35,25 +35,149 @@ library(raster)
 library(xts)
 library(caTools)  
 
+# A function to get the rate of change of a vector
+gSlope <- function(x, na.rm=TRUE){
+	if(sum(!is.na(x))<3){
+		NA
+	}else{
+		as.numeric(lm(x~I(0:(length(x)-1)))$coef[2])
+	}
+}
+
+# A function to define the 3x3 grid
+# See grid here:
+# http://webhelp.esri.com/arcgisdesktop/9.2/index.cfm?topicname=how_slope_works
+
+
+
+# =====================
+# = Read and trim SST =
+# =====================
 startYear <- 1968   # start of the period
-endYear <- 2011     # end of the period
+endYear <- 2013     # end of the period
 subp <- '1968-01-01/2012-12-01'   # period for the climatology calculation
 
 
+# First, read in the brick
+sst000 <- brick("/Users/Battrd/Documents/School&Work/pinskyPost/trawl/Data/raw_data/HadISST/HadISST_sst.nc")
 
-sst00 <- brick("/Users/Battrd/Documents/School&Work/pinskyPost/trawl/Data/raw_data/HadISST/HadISST_sst.nc")
-Date <- substr(names(sst00),2,11) 
-Date <- gsub('\\.', '\\-', Date)
-Date <- as.Date(Date)
+# Define desired starting and stopping dates, and define dates in brick
+Date <- substr(names(sst000),2,11) # get brick dates
+Date <- gsub('\\.', '\\-', Date) # format brick dates by replacing "." with "-"
+Date <- as.Date(Date) # format brick dates as a date object
 
-dstart.pat <- paste(startYear,'01','[0-9]{2}',sep='-')
-dstart <- grep(dstart.pat, as.character(Date))
+dstart.pat <- paste(startYear,'01','[0-9]{2}',sep='-') # define the start date pattern to be used in a regular expression
+dstart <- grep(dstart.pat, as.character(Date)) # find the starting date pattern in the brick dates
 
-dend.pat <- paste(endYear,'12','[0-9]{2}',sep='-')
-dend <- grep(dend.pat, Date)
+dend.pat <- paste(endYear,'12','[0-9]{2}',sep='-') #define the end date pattern for the regex
+dend <- grep(dend.pat, Date) # find the ending date pattern in the brick dates
 
-sst0 <- subset(sst00, dstart:dend)
-Date <- Date[dstart:dend]
+
+# Second, trim the brick to the desired dates
+sst00 <- subset(sst000, dstart:dend) # subset the brick to the dates within (inclusively) the start and end dates
+Date <- Date[dstart:dend] # trim the brick dates to match the brick
+
+
+# Third, trim the brick to the desired coordinates
+xmin(sst00) <- xmin(sst00) - 30
+xmax(sst00) <- xmax(sst00) + 30 
+newExtent <- extent(c(-210, -30, 20, 70)) # define the new "extent"
+sst0 <- crop(sst00, newExtent)
+
+
+# Fourth, ice and land values as NA's
+# http://stackoverflow.com/questions/11966503/how-to-replace-nas-in-a-raster-object
+sst <- reclassify(sst0, matrix(c(-1000, -32768, NA, NA), ncol=2))
+
+# Fifth, take annual means.
+sst.ann <- stackApply(sst, indices=rep(1:(dim(sst)[3]/12),each=12), fun=mean)
+
+# Sixth, take grand means
+sst.mu <- stackApply(sst.ann, indices=rep(1, length(sst.ann)), fun=mean)
+
+# ==================================
+# = Get the temporal trend for SST =
+# ==================================
+blah <- as.array(sst.ann)
+blah[1,1,]
+timeTrend <- stackApply(sst.ann, indices=rep(1,length(sst)), fun=gSlope)
+
+
+# ============================
+# = Get the spatial gradient =
+# ============================
+
+# blah <- as.matrix(timeTrend)
+# #  blah[25:27,35:37]
+# #             [,1]        [,2]        [,3]
+# # [1,] 0.008696129 0.008692858 0.009128356
+# # [2,] 0.007054508 0.007128589 0.007470006
+# # [3,] 0.006070242 0.006230357 0.006470819
+#
+# blah2 <- as.vector(blah)
+# ind.26.36 <- nrow(blah)*35+26 # get the value at 26, 36
+# blah2[ind.26.36] # ok, the index of 26,36 is 1776
+# blah[ind.26.36-1] # go directly above the focal index
+
+cent1mat <- matrix(c(rep(0,4),1,rep(0,4)), ncol=3)
+f.padVal <- focal(sst.mu, w=cent1mat, fun=sum)
+w.dzdx <- matrix(c(-1,-2,-1,0,0,0,1,2,1), ncol=3)
+w.dzdy <- matrix(c(-1,0,1,-2,0,2,-1,0,1), ncol=3)
+
+
+
+spatGrad.dzdx <- 
+
+
+
+
+
+
+
+#
+# aiGrid <- function(x, focal.row, focal.col){
+# 	dx <- dim(x)
+# 	nr <- dx[1]
+# 	nc <- dx[2]
+#
+# 	grid.out <- c("a"=NaN, "b"=NaN, "c"=NaN, "d"=NaN, "e"=NaN, "f"=NaN, "g"=NaN, "h"=NaN, "i"=NaN)
+#
+# 	# Simple case, where the focal element is not on the edge
+# 	if(!focal.row%in%c(1, nr) & !focal.col%in%c(1, nc)){
+# 		grid.33 <- as.matrix(x[(focal.row-1):(focal.row+1), (focal.col-1):(focal.col+1)])
+# 		grid.out[names(grid.out)] <- c(t(grid.33))
+# 	}
+#
+# 	# When the focal element is at either the minimum or maximum row
+# 	if(focal.row%in%c(1, nr) & !focal.col%in%c(1, nc)){
+# 		if(focal.row==1L){ # if at the minimum (top) row
+# 			grid.23 <- as.matrix(x[(focal.row):(focal.row+1), (focal.col-1):(focal.col+1)])
+# 			grid.out[letters[4:9]] <- c(t(grid.23))
+# 		}else{ # if at the maximum (bottom) row
+# 			grid.23 <- as.matrix(x[(focal.row-1):(focal.row), (focal.col-1):(focal.col+1)])
+# 			grid.out[letters[1:6]] <- c(t(grid.23))
+# 		}
+# 	}
+#
+# 	# When the focal element is at either the minimum or maximum column
+# 	if(!focal.row%in%c(1, nr) & focal.col%in%c(1, nc)){
+# 		if(focal.col==1L){ # if at the minimum (left) column
+# 			grid.32 <- as.matrix(x[(focal.row-1):(focal.row+1), (focal.col):(focal.col+1)])
+# 			grid.out[c("b","c","e","f","h","i")] <- c(t(grid.32))
+# 		}else{ # if at the maximum (bottom) row
+# 			grid.23 <- as.matrix(x[(focal.row-1):(focal.row+1), (focal.col-1):(focal.col)])
+# 			grid.out[c("a","b","d","e","g","h")] <- c(t(grid.32))
+# 		}
+# 	}
+#
+#
+#
+# 	focal.row.m1 <-
+#
+# 	grid.33 <- x[]
+#
+# }
+
 
 tserie <- as.vector(extract(sst, cbind(116, -35)))
 tserie <- xts(tserie, order.by=Date)
