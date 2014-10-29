cb57f8fd8e16c2badd65d08a6c7a60a03b3252e5
rbatt <battrd@gmail.com>
2014-10-29 09:47:10 -0400

    dead end w/ focal(), discovered slope() I had previous been pursuing creative use of focal and its weights argument, and its padValue argument. However, upon realizing that it would be extremely difficult to get padValue to change with the focal cell (as the the value in the focal cell should be used to pad missing/ past-edge values, as per ArcGIS), I started looking for alternatives. Found the SDMTools package, which has C functions to calculate slope and aspect based on the Burrough and McDonell 1998, the same reference as in ArcGIS. Looking at the C source for these functions (slope.aspect.c), it is clear that the author was follow the B&M 1998 method exactly (or at least intending to do so). So I'm going to simply switch over to this approach. After this commit, will clean up code. Will move analysis to own script.

--- Scripts/Data/read.HadISST.R
+++ Scripts/Data/read.HadISST.R
@@ -89,17 +89,18 @@ sst0 <- crop(sst00, newExtent)
 # http://stackoverflow.com/questions/11966503/how-to-replace-nas-in-a-raster-object
 sst <- reclassify(sst0, matrix(c(-1000, -32768, NA, NA), ncol=2))
 
+
 # Fifth, take annual means.
 sst.ann <- stackApply(sst, indices=rep(1:(dim(sst)[3]/12),each=12), fun=mean)
+
 
 # Sixth, take grand means
 sst.mu <- stackApply(sst.ann, indices=rep(1, length(sst.ann)), fun=mean)
 
+
 # ==================================
 # = Get the temporal trend for SST =
 # ==================================
-blah <- as.array(sst.ann)
-blah[1,1,]
 timeTrend <- stackApply(sst.ann, indices=rep(1,length(sst)), fun=gSlope)
 
 
@@ -119,14 +120,20 @@ timeTrend <- stackApply(sst.ann, indices=rep(1,length(sst)), fun=gSlope)
 # blah2[ind.26.36] # ok, the index of 26,36 is 1776
 # blah[ind.26.36-1] # go directly above the focal index
 
+testGrid <- raster(matrix(c(50,30,8,45,30,10,50,30,10), ncol=3))
+
 cent1mat <- matrix(c(rep(0,4),1,rep(0,4)), ncol=3)
 f.padVal <- focal(sst.mu, w=cent1mat, fun=sum)
 w.dzdx <- matrix(c(-1,-2,-1,0,0,0,1,2,1), ncol=3)
 w.dzdy <- matrix(c(-1,0,1,-2,0,2,-1,0,1), ncol=3)
 
+blah <- focal(testGrid, w.dzdx, sum)
+blah <- focal(testGrid, w.dzdy, sum)
 
-
-spatGrad.dzdx <- 
+library(SDMTools)
+# NOTE: I have now discovered SDMTools:::slope(), and am no longer going to try to write my own function for doing this calculation via focal. See later versions of this script for how this line of thinking progresses. (RDB, 29-Oct-2014)
+spatGrad.slope <- slope(sst.mu, latlon=TRUE)
+spatGrad.aspect <- aspect(sst.mu, latlon=TRUE)
 
 
 
