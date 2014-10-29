230c5e798ddbb3667d88625dc1d86b2eed8c3543
rbatt <battrd@gmail.com>
2014-10-29 19:15:54 -0400

    calculated trajectories, ignoring coastline issues Trajectory calculations are probably good for trajectories that never come into contact with coast. Started logic sequence for selecting a new cell if previous was non-NA velocity, but proposed cell had NA velocity. Nonetheless, barebones loop is in place.

--- Scripts/Analysis/burrowsHadISST.R
+++ Scripts/Analysis/burrowsHadISST.R
@@ -1,3 +1,4 @@
+
 # =================
 # = Load Packages =
 # =================
@@ -5,45 +6,139 @@ library(raster)
 library(SDMTools)
 
 
-
 # =============
 # = Load Data =
 # =============
 load("/Users/Battrd/Documents/School&Work/pinskyPost/trawl/Data/HadISST.RData")
 
+
+# =============================
+# = Load statistics functions =
+# =============================
+stat.location <- "~/Documents/School&Work/pinskyPost/trawl/Scripts/StatFunctions"
+invisible(sapply(paste(stat.location, list.files(stat.location), sep="/"), source, .GlobalEnv))
+
+
 # ==================================
 # = Get the temporal trend for SST =
 # ==================================
-timeTrend <- stackApply(sst.ann, indices=rep(1,length(sst)), fun=gSlope)
+timeTrend <- stackApply(sst.ann, indices=rep(1,length(sst)), fun=sstTimeSlope)
 
 
 # ============================
 # = Get the spatial gradient =
 # ============================
-
-# blah <- as.matrix(timeTrend)
-# #  blah[25:27,35:37]
-# #             [,1]        [,2]        [,3]
-# # [1,] 0.008696129 0.008692858 0.009128356
-# # [2,] 0.007054508 0.007128589 0.007470006
-# # [3,] 0.006070242 0.006230357 0.006470819
-#
-# blah2 <- as.vector(blah)
-# ind.26.36 <- nrow(blah)*35+26 # get the value at 26, 36
-# blah2[ind.26.36] # ok, the index of 26,36 is 1776
-# blah[ind.26.36-1] # go directly above the focal index
-
-testGrid <- raster(matrix(c(50,30,8,45,30,10,50,30,10), ncol=3))
-
-cent1mat <- matrix(c(rep(0,4),1,rep(0,4)), ncol=3)
-f.padVal <- focal(sst.mu, w=cent1mat, fun=sum)
-w.dzdx <- matrix(c(-1,-2,-1,0,0,0,1,2,1), ncol=3)
-w.dzdy <- matrix(c(-1,0,1,-2,0,2,-1,0,1), ncol=3)
-
-blah <- focal(testGrid, w.dzdx, sum)
-blah <- focal(testGrid, w.dzdy, sum)
-
-library(SDMTools)
-# NOTE: I have now discovered SDMTools:::slope(), and am no longer going to try to write my own function for doing this calculation via focal. See later versions of this script for how this line of thinking progresses. (RDB, 29-Oct-2014)
 spatGrad.slope <- slope(sst.mu, latlon=TRUE)
-spatGrad.aspect <- aspect(sst.mu, latlon=TRUE)
+spatGrad.aspect <- aspect(sst.mu, latlon=FALSE)
+reclassify(spatGrad.aspect, cbind(-1, NA))
+
+
+# ===========================
+# = Calculate Climate Speed =
+# ===========================
+climSpeed0 <- timeTrend/spatGrad.slope # climate speed in km/yr
+
+spdX0 <- climSpeed*sin(spatGrad.aspect) # in km/yr (to the east)
+spdY0 <- climSpeed*cos(spatGrad.aspect) # in km/yr (to the north)
+
+# heat.cols <- colorRampPalette(c("#000099", "#00FEFF", "#45FE4F", "#FCFF00", "#FF9400", "#FF3100"))(256)
+# plot(climSpeed0, col=heat.cols)
+# plot(spdX0, col=heat.cols)
+# plot(spdY0, col=heat.cols)
+
+
+# ==========================
+# = Calculate Trajectories =
+# ==========================
+n.per.yr <- 2
+n.per.ll <- 3
+
+n.yrs <- dim(sst.ann)[3] # number of years
+step.index <- seq(2, n.yrs*n.per.yr, length.out=n.yrs*n.per.yr-1) # time counter for loop
+tYrs <- rep(1:n.yrs, each=n.per.yr) # reference to the year # that lines up with step.index
+
+# Expand rasters
+spdX <- disaggregate(spdX0, 3) # fine spatial resolution for longitudinal climate speed
+spdY <- disaggregate(spdY0, 3) # fine spatial resolution for latitudinal climate speed
+ang <- disaggregate(spatGrad.aspect0, 3) # final spatial resolution for the angle of climate velocity
+
+#
+
+
+
+trajLon <- brick(array(NA, dim=dim(sst.ann)*c(n.per.ll,n.per.ll,n.per.yr)), xmn=-190, xmx=-40, ymn=20, ymx=65)
+lons <- seq(xmin(trajLon), xmax(trajLon), length.out=ncol(trajLon))
+trajLon <- setValues(trajLon, rep(lons, nrow(trajLon)), layer=1)
+
+# extract(trajLon,as.matrix(expand.grid(-180:-170, 30:40)), layer=1, nl=1, cellnumbers=TRUE)
+
+trajLat <- brick(array(NA, dim=dim(sst.ann)*c(n.per.ll,n.per.ll,n.per.yr)), xmn=-190, xmx=-40, ymn=20, ymx=65)
+lats <- seq(ymax(trajLat), ymin(trajLat), length.out=ncol(trajLat))
+trajLat <- setValues(trajLat, rep(lats, each=nrow(trajLat)), layer=1)
+
+# practice matrix subsetting matrix
+# test <- matrix(0, ncol=3, nrow=3)
+# test[matrix(c(1:3, 1:3),ncol=2)] <- c(4,5,6)
+
+dXkm.s <- spdX*(1/n.per.yr) # "small" (per-time-step) change in X
+dYkm.s <- spdY*(1/n.per.yr) # "small" (per-time-step) change in Y
+
+t.dest.dX <- dXkm.s # the X speed in the previous location 
+t.dest.dY <- dYkm.s # they Y speed in the previous location
+
+sst.pb <- txtProgressBar(min=2, max=max(step.index), style=3)
+for(i in step.index){
+	
+	# Calculate the longitude and latitude of proposed destination
+	t.X.lon <- subset(trajLon, i-1) + t.dest.dX/111.325*cos(subset(trajLat, i-1))
+	t.Y.lat <- subset(trajLat, i-1) + t.dest.dY/111.325
+	
+	# Extract cexll# and X/Y speeds of proposed cell
+	t.LL <- cbind(values(t.X.lon), values(t.Y.lat)) # format proposed LL
+	ex.dX <- extract(dXkm.s, t.LL, cellnumbers=TRUE) # extract X speed and cell # of cell
+	ex.dY <- extract(dYkm.s, t.LL) # extract Y speed of proposed cell (cell numbers are the same for ex.dX and ex.dY)
+	
+	# Insert check to make sure not crossing boundary
+	# If the destination X velocity is NA, so too is the Y velocity, probably; could probably just check for either
+	if(!is.na(t.dest.dX) & is.na(ex.dX[,2])){ # if moving from cell with non-NA to NA velocity ...
+		if(t.dest.dX>0){ # if the velocity was positive, search for lowest temp non-diagonal neighbor
+			# need to get non-diagonal focal temperatures, find the index of the lowest temp
+		}else{
+			# if the velocity is negative, then find the highest temp
+		}
+		# need
+	}
+	
+	# Update dest speeds to reflect those in proposed cell
+	t.dest.dX <- setValues(t.dest.dX, ex.dX[,2])
+	t.dest.dY <- setValues(t.dest.dY, ex.dY)
+	
+	# Update the destination LL in the trajectories (not rounded to correspond to cell)
+	trajLon <- setValues(trajLon, t.X.lon, layer=i)
+	trajLat <- setValues(trajLat, t.Y.lat, layer=i)
+	
+	# Extract LL of cell corresponding to proposed location
+	new.LL <- xyFromCell(dXkm.s, ex.dX[,1])
+	
+	
+	setTxtProgressBar(sst.pb, i)
+}
+
+
+
+
+# Create a raster for each of lat and lon values
+
+# Create TrajArrive brick w/ # layers = # years
+	# 
+
+
+# Calculate x and y displacements in km
+
+# Convert displacements
+
+
+
+
+
+
