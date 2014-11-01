3f0c430d2710007bc5eb81c9f89132d5f7de379e
rbatt <battrd@gmail.com>
2014-11-01 19:56:48 -0400

    getting a warning message, but old function same
    I did a bunch of benchmarking work, then went to run the new code on the hadisst data, and got a warning. Adding in old function (as well as switching which half of the hybrid was being implemented) did not resolve the issue.
    
    
    Former-commit-id: ea761829787e78033d771b4952610216b61a5d82

--- Scripts/StatFunctions/timeSlope.R
+++ Scripts/StatFunctions/timeSlope.R
@@ -1,5 +1,11 @@
 # The fruit of: https://github.com/rBatt/trawl/commit/a04d8e166d7093538c698368d48542a0e60bd16c
 timeSlope<- function(x, ...){
+	# Old function
+	# if(sum(!is.na(x))<3){
+	# 	NA
+	# }else{
+	# 	as.numeric(lm(x~I(0:(length(x)-1)))$coef[2])
+	# }
 	nona <- !is.na(x)
 	if(sum(nona)<3){
 		NA
@@ -9,7 +15,7 @@ timeSlope<- function(x, ...){
 			tvec <- (1:lx)[nona]
 			x2 <- x[nona]
 			cor(x2, tvec)*(sd(x2)/sd(tvec))
-			
+
 		}else{
 			tvec <- cbind(1,1:lx)[nona,] # the 1 is a dummy vector for the intercept
 			ttvec <- t(tvec)
