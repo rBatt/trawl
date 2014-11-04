c2b83ce0a6f61bdaada003188119aa557f9681ab
rbatt <battrd@gmail.com>
2014-11-04 13:55:34 -0500

    spacing to emphasize that limitV.mat defined here
    also removed misleading comment
    
    
    Former-commit-id: 3875887ac90a4c6947ac116614a9a522b40a4c8e

--- Scripts/StatFunctions/limitV.R
+++ Scripts/StatFunctions/limitV.R
@@ -1,10 +1,12 @@
 # Limit a velocity (provided in km/timeStep) to 1º of lat or lon per timeStep
 # Note that 1 variable is defined outside of the function – this is to reduce computation time
 # Also note that conv.fact.lon needs to be defined inside the script; again, this is to reduce computation time
+
 limitV.mat <- cbind(c(1,-Inf),c(Inf,-1),c(1,-1))
+
 limitV <- function(r, dir="lon"){ # limit velocity
 	if(dir=="lon"){
-		r.coord <- r/conv.fact.lon # when coords is lats
+		r.coord <- r/conv.fact.lon
 		r.coord <- reclassify(r.coord, limitV.mat)
 		r <- r.coord*conv.fact.lon
 	}else{
