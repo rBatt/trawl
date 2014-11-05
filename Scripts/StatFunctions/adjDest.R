025f304324288915b3c90702ae9f6210fdd28cce
rbatt <battrd@gmail.com>
2014-11-05 13:29:00 -0500

    cleaning adjDest; error missing cool/warmLon/Lat
    There are places where startLon or startLat are not NA, but cool- warmLon/Lat are NA. Obviously this would happen over land. Actually, now that I'm looking at graphs more carefullly, I think this is only happening over land (and maybe the great lakes as well). So maybe this is a problem that coul dbe fixed by replacing places with missing startVel with the startLon or startLat
    
    Good news is that this isn't a problem with the resolution or mismatch between spatial - temporal gradients and the sst's or anything like that. (I just fixed that issue!)
    
    
    Former-commit-id: 00a43114f83577e9926ee07e8d87b663fe871497 [formerly 661014579a7f8ef572e33151ff927287e9ffb2c7]
    Former-commit-id: 5779d7f1588ec35906b0200e4e9378065796a2ab

--- Scripts/StatFunctions/adjDest.R
+++ Scripts/StatFunctions/adjDest.R
@@ -1,3 +1,4 @@
+# Function to select the right layer from rook-based velocities given a raster containing focal (rook) cell#'s
 focal2dL <- function(f, dL){
 	# f contains the focal rook matches
 	# dL contains either latitude or longitude velocities associated with those rook directions
@@ -7,8 +8,78 @@ focal2dL <- function(f, dL){
 	v8 <- (f==8L)*subset(dL, 4)
 	v4+v6+v2+v8
 }
+
+# dest.cell.LL <- adjDest(
+# 	startLon=start.lon,
+# 	startLat=start.lat,
+# 	startCell=start.cell,
+# 	startVel=destV, # note that this is the destination velocity from the previous time step (thus, starting velocity)
+# 	startTemp=start.temp,
+#
+# 	propTemp=prop.temp,
+# 	propCell=prop.cell,
+# 	propLL=prop.LL,
+#
+# 	rook.dLon=dest.dX.rook/111.325*cos(start.lat/180*pi),
+# 	rook.dLat=dest.dY.rook/111.325
+# )
+
+dev.new()
+par(mfrow=c(2,2))
+plot(is.finite(sst.mu2)&!is.finite(spatAng0))
+plot(is.finite(sst.mu2)&!is.finite(spatAng.03))
+plot(is.finite(sst.mu2)&!is.finite(spatAng.f7))
+plot(is.finite(sst.mu2)&!is.finite(spatAng))
+
+par(mfrow=c(2,2))
+plot(is.finite(sst.mu2)&!is.finite(spatSlope0))
+plot(is.finite(sst.mu2)&!is.finite(spatSlope.03))
+plot(is.finite(sst.mu2)&!is.finite(spatSlope.f7))
+plot(is.finite(sst.mu2)&!is.finite(spatSlope))
+
+plot(is.finite(sst.mu2)&!is.finite(climV))
+
+
+par(mfrow=c(2,2))
+plot(is.finite(sst.mu2)&!is.finite(subset(rook.dLon,1)))
+plot(is.finite(sst.mu2)&!is.finite(subset(rook.dLon,2)))
+plot(is.finite(sst.mu2)&!is.finite(subset(rook.dLon,3)))
+plot(is.finite(sst.mu2)&!is.finite(subset(rook.dLon,4)))
+
+par(mfrow=c(2,2))
+plot(is.finite(sst.mu2)&!is.finite(subset(rook.dLat,1)))
+plot(is.finite(sst.mu2)&!is.finite(subset(rook.dLat,2)))
+plot(is.finite(sst.mu2)&!is.finite(subset(rook.dLat,3)))
+plot(is.finite(sst.mu2)&!is.finite(subset(rook.dLat,4)))
+
+par(mfrow=c(2,2))
+plot(is.finite(sst.mu2)&!is.finite(cool.dLon), main="cool.dLon")
+plot(is.finite(sst.mu2)&!is.finite(cool.dLat), main="cool.dLat")
+plot(is.finite(sst.mu2)&!is.finite(warm.dLon), main="warm.dLon")
+plot(is.finite(sst.mu2)&!is.finite(warm.dLat), main="warm.dLat")
+
+par(mfrow=c(2,2))
+plot(is.finite(sst.mu2)&!is.finite(coolLon), main="coolLon")
+plot(is.finite(sst.mu2)&!is.finite(coolLat), main="coolLat")
+plot(is.finite(sst.mu2)&!is.finite(warmLon), main="warmLon")
+plot(is.finite(sst.mu2)&!is.finite(warmLat), main="warmLat")
+
+
+par(mfrow=c(2,2))
+plot(is.finite(startVel)&!is.finite(coolLon), main="coolLon")
+plot(is.finite(startVel)&!is.finite(coolLat), main="coolLat")
+plot(is.finite(startVel)&!is.finite(warmLon), main="warmLon")
+plot(is.finite(startVel)&!is.finite(warmLat), main="warmLat")
+
+par(mfrow=c(2,2))
+plot(is.finite(startLon)&!is.finite(coolLon), main="coolLon")
+plot(is.finite(startLat)&!is.finite(coolLat), main="coolLat")
+plot(is.finite(startLon)&!is.finite(warmLon), main="warmLon")
+plot(is.finite(startLat)&!is.finite(warmLat), main="warmLat")
+
+
 # Function to adjust the proposed destination of the trajectory, if it needs it.
-adjDest <- function(startVel, startTemp, propTemp, startCell, propCell, propLL){
+adjDest <- function(startLon, startLat, startCell, startVel, startTemp, propTemp, propCell, propLL, rook.dLon, rook.dLat){
 	# Find coolest and warmest rook neighbors (location and temperature)
 	coolFocal <- focal.min(startTemp) # gets the cell# within the focal matrix around startTemp
 	cool.dLon <- focal2dL(f=coolFocal, dL=rook.dLon)
@@ -16,13 +87,18 @@ adjDest <- function(startVel, startTemp, propTemp, startCell, propCell, propLL){
 	coolLon <- startLon + cool.dLon
 	coolLat <- startLat + cool.dLat
 	coolLL <- cbind(values(coolLon), values(coolLat))
-	coolCell <- 
-	coolCell <- focal2cell(coolFocal) # get convert the cell # in the focal (3x3) raster to the cell# in the full raster
-	coolTemp <- setValues(startTemp, extract(startTemp,values(coolCell))) # temp of coolest rook neighbor
+	coolCell <- cellFromXY(startTemp, coolLL)
+	coolTemp <- setValues(startTemp, extract(startTemp,coolCell)) # temp of coolest rook neighbor
 	
-	warmFocal <- focal.max(startTemp) # gets the cell # within the focal matrix
-	warmCell <- focal2cell(warmFocal) # convert focal cell # to full-raster cell#
-	warmTemp <- setValues(startTemp, extract(startTemp,values(warmCell))) # temp of warmest rook neighbor
+	warmFocal <- focal.max(startTemp) # gets the cell# within the focal matrix around startTemp
+	warm.dLon <- focal2dL(f=warmFocal, dL=rook.dLon)
+	warm.dLat <- focal2dL(f=warmFocal, dL=rook.dLat)
+	warmLon <- startLon + warm.dLon
+	warmLat <- startLat + warm.dLat
+	warmLL <- cbind(values(warmLon), values(warmLat))
+	warmCell <- cellFromXY(startTemp, warmLL)
+	warmTemp <- setValues(startTemp, extract(startTemp,warmCell)) # temp of warmest rook neighbor
+	
 	
 	posVel <- is.finite(startVel)&startVel>0
 	negVel <- is.finite(startVel)&startVel<0
@@ -36,7 +112,7 @@ adjDest <- function(startVel, startTemp, propTemp, startCell, propCell, propLL){
 	
 	# A cell should not be TRUE in more than one of the bel___ rasters; if it does, my logic is flawed
 	sumBelongs <- values(belProp+belCool+belWarm+belStart) # the number adjustment outcomes to which each cell belongs
-	stopifnot(is.na(sumBelongs)|sumBelongs<=1) # check to ensure that *at most* 1 adjustment is made to each proposed trajectory
+	stopifnot(all(is.na(sumBelongs)|sumBelongs<=1)) # check to ensure that *at most* 1 adjustment is made to each proposed trajectory
 	
 	# Rasters can be easily added, and it is easy to define a cell as T/F/NA
 	# T/F are interpreted as 1/0
@@ -44,10 +120,15 @@ adjDest <- function(startVel, startTemp, propTemp, startCell, propCell, propLL){
 	# This provides means to do easy conditional replacement/ matching
 	# Multiply each category of outcome w/ the appropriate logic, add products
 	# This applies the appropriate adjustment to each proprosed trajectory that needs adjusting (values are cell#)
-	destCell <- belProp*propCell + belStart*startCell + belCool*coolCell + belWarm*warmCell # checked/adjusted destination cell
+	# destCell <- belProp*propCell + belStart*startCell + belCool*coolCell + belWarm*warmCell # checked/adjusted destination cell
 	
 	destLL <- propLL
-	destLL[values(belAdj),] <- xyFromCell(destCell, values(destCell))[values(belAdj),]
+	destLL[values(belCool),] <- coolLL[values(belCool),]
+	destLL[values(belWarm),] <- warmLL[values(belCool),]
+	destLL[values(belStart),] <- cbind(values(startLon), values(startLat))[values(belCool),]
 	
-	return(list(cell=destCell, LL=destLL))
+	# destLL[values(belAdj),] <- xyFromCell(destCell, values(destCell))[values(belAdj),]
+	
+	# return(list(cell=destCell, LL=destLL))
+	return(destLL)
 }
