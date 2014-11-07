ad15b1122e7b8b16eccf3a3e4753b8e3f91ed2e8
rbatt <battrd@gmail.com>
2014-11-06 23:42:53 -0500

    limit rook velocities to make sense w/ direction
    limitV once again only works with the rook raster because it subsets to the layers
    
    the change says that if we're dealing with longitude, up and down velocities have to be 0, and if we're dealing with latitude, left and right velocities have to be 0.
    
    In the future, might also change this to for the correct sign convention (b/c the rook already defines the direction so that all that i need to do is get the speed, not the 'velocity')
    
    
    Former-commit-id: 4d23e7da809836f2da1ef35fc008b251e4766a47 [formerly 9721a3e9174d6909c8fd68664a80f90bdc8cf7db]
    Former-commit-id: 7600fc3a61d01e126662a4e8ddd8a66348b6e53f

--- Scripts/StatFunctions/limitV.R
+++ Scripts/StatFunctions/limitV.R
@@ -2,8 +2,8 @@
 # Note that 1 variable is defined outside of the function – this is to reduce computation time
 # Also note that conv.fact.lon needs to be defined inside the script; again, this is to reduce computation time
 
-# limitV.mat <- cbind(c(1,-Inf),c(Inf,-1),c(1,-1))
-limitV.mat <- cbind(c(1,-Inf),c(Inf,-1),c(1,0))
+limitV.mat <- cbind(c(1/n.per.ll,-Inf),c(Inf,-1/n.per.ll),c(1/n.per.ll,-1/n.per.ll))
+# limitV.mat <- cbind(c(1,-Inf),c(Inf,-1),c(1,0))
 
 limitV <- function(r, dir="lon", conv.fact.lon=NULL){ # limit velocity
 	if(dir=="lon"){
@@ -11,10 +11,14 @@ limitV <- function(r, dir="lon", conv.fact.lon=NULL){ # limit velocity
 		r.coord <- r/conv.fact.lon
 		r.coord <- reclassify(r.coord, limitV.mat)
 		r <- r.coord*conv.fact.lon
+		r <- setValues(r, rep(0, ncell(r)), layer=3) # 3rd layer is up, if dealing with longitude, vel should be 0
+		r <- setValues(r, rep(0, ncell(r)), layer=4) # 4th layer is down, if dealing with longitude, vel should be 0
 	}else{
 		r.coord <- r/111.325
 		r.coord <- reclassify(r.coord, limitV.mat)
 		r <- r.coord*111.325
+		r <- setValues(r, rep(0, ncell(r)), layer=1) # 1st layer is left, if dealing with latitude, vel should be 0
+		r <- setValues(r, rep(0, ncell(r)), layer=2) # 2nd layer is right, if dealing with latitude, vel should be 0
 	}
 	r
 }
