23b5fc57d4f0a087a1e5cc3b0cfebccf5f29071b
rbatt <battrd@gmail.com>
2014-11-07 15:11:59 -0500

    round focal min/max to ensure better match
    Not sure why I'm having to do this
    
    
    Former-commit-id: 891ab1d147b00193ab4e1e7f7ac3fc127f7b0b89 [formerly 0bcfac062cdd8f1a0082acdcb345f7c5e104634a]
    Former-commit-id: d2b2d54fe87e2e0590537c9226103ab68697c83a

--- Scripts/StatFunctions/focal.max.R
+++ Scripts/StatFunctions/focal.max.R
@@ -1,5 +1,6 @@
 
 focal.max <- function(r.max){ # where is the biggest value in the rook focus?
 	# fw.mat <- matrix(c(NA,1,NA,1,NA,1,NA,1,NA),ncol=3) # focal weight matrix
-	focal(r.max, w=fw.mat, which.max, pad=TRUE, padValues=-Inf) # focal raster cell# containing biggest value
+	f0 <- focal(r.max, w=fw.mat, which.max, pad=TRUE, padValues=-Inf) # focal raster cell# containing biggest value
+	setValues(f0, round(values(f0)))
 }
