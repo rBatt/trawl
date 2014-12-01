b6440aaf7251fa3319156009e522670a577fe19f
rbatt <battrd@gmail.com>
2014-12-01 10:35:04 -0500

    add tolerance for goa 1º
    not having a tolerance for missingness leads to the eastern part of the region being completely absent
    
    
    Former-commit-id: 1e8bd77eeba9056b1d927d8496551cea14a423c6 [formerly 62d8881e7b497a2447eddac14bfd5307c3e870e6 [formerly dbaf95723598591c8fdd85df2e91d0c7f45250c3]]
    Former-commit-id: 5663e80bd31e3eae413ea4006cfd43f95c511745
    Former-commit-id: 8f79c47fc7fc52ef9dad1fd02f1a2c31ef43eeac

--- Scripts/Data/read.goa.R
+++ Scripts/Data/read.goa.R
@@ -86,7 +86,77 @@ goa[,strat2:=paste(stratum, ll2strat(lon, lat))]
 # goa[,strat2:=paste(stratum, ll2strat(lon, lat, 0.25))]
 # goa[,sum(colSums(table(year, strat2)>0)==nyears)] # 0.25º grid gives you 6 strata seen every year
 
-goodStrat2 <- goa[,names(colSums(table(year, strat2)>0))[colSums(table(year, strat2)>0)==nyears]]
+
+
+nstrata <- c()
+nstrata.orig <- c()
+for(i in 0:(nyears-1)){
+	nstrata[i+1] <- goa[,sum(colSums(table(year, strat2)>0)>=(nyears-i))]
+	nstrata.orig[i+1] <- goa[,sum(colSums(table(year, stratum)>0)>=(nyears-i))]
+}
+dev.new(width=4)
+par(mfrow=c(2,1), mar=c(2.5,2,1.5,0.2), cex=1, ps=10, mgp=c(1.25, 0.15, 0), tcl=-0.25)
+plot(0:(nyears-1), nstrata, type="o", xlab="threshold # years missing", ylab="# strata below threshold missingness", main="# strata vs. tolerance of missingness")
+lines(0:(nyears-1), nstrata.orig, type="o", col="red")
+legend("topleft", legend=c("original strata definition", "1 degree grid definition"), lty=1, pch=21, col=c("red","black"))
+image(x=goa[,sort(unique(year))], y=goa[,1:length(unique(strat2))], z=goa[,table(year, strat2)>0], xlab="year", ylab="1 degree stratum ID", main="stratum presence vs. time; red is absent")
+
+
+
+lat.range <- goa[,range(lat, na.rm=TRUE)]
+lon.range <- goa[,range(lon, na.rm=TRUE)]
+
+dev.new(width=3, height=8.5)
+par(mfrow=c(6,1), mar=c(1.25,1.25,0.1,0.1), mgp=c(1,0.15,0), tcl=-0.15, ps=8, cex=1, family="Times")
+tol0 <- goa[strat2%in%goa[,names(colSums(table(year, strat2)>0))[colSums(table(year, strat2)>0)>=(nyears)]]]
+tol0[,c("lat","lon"):=list(roundGrid(lat),roundGrid(lon))]
+setkey(tol0, lat, lon)
+tol0 <- unique(tol0)
+tol0[,plot(lon, lat, xlab="", ylab="", xlim=lon.range, ylim=lat.range)]
+legend("topleft", "0 missing years permitted", inset=c(-0.1, -0.12), bty="n")
+
+tol1 <- goa[strat2%in%goa[,names(colSums(table(year, strat2)>0))[colSums(table(year, strat2)>0)>=(nyears-1)]]]
+tol1[,c("lat","lon"):=list(roundGrid(lat),roundGrid(lon))]
+setkey(tol1, lat, lon)
+tol1 <- unique(tol1)
+tol1[,plot(lon, lat, xlab="", ylab="", xlim=lon.range, ylim=lat.range, col=1+(!paste(lon,lat)%in%tol0[,paste(lon,lat)]))]
+legend("topleft", "1 missing year permitted", inset=c(-0.1, -0.12), bty="n")
+
+tol2 <- goa[strat2%in%goa[,names(colSums(table(year, strat2)>0))[colSums(table(year, strat2)>0)>=(nyears-2)]]]
+tol2[,c("lat","lon"):=list(roundGrid(lat),roundGrid(lon))]
+setkey(tol2, lat, lon)
+tol2 <- unique(tol2)
+tol2[,plot(lon, lat, xlab="", ylab="", xlim=lon.range, ylim=lat.range, col=1+(!paste(lon,lat)%in%tol0[,paste(lon,lat)]))]
+legend("topleft", "2 missing years permitted", inset=c(-0.1, -0.12), bty="n")
+
+
+tol3 <- goa[strat2%in%goa[,names(colSums(table(year, strat2)>0))[colSums(table(year, strat2)>0)>=(nyears-3)]]]
+tol3[,c("lat","lon"):=list(roundGrid(lat),roundGrid(lon))]
+setkey(tol3, lat, lon)
+tol3 <- unique(tol3)
+tol3[,plot(lon, lat, xlab="", ylab="", xlim=lon.range, ylim=lat.range, col=1+(!paste(lon,lat)%in%tol0[,paste(lon,lat)]))]
+legend("topleft", "3 missing years permitted", inset=c(-0.1, -0.12), bty="n")
+
+
+tol4 <- goa[strat2%in%goa[,names(colSums(table(year, strat2)>0))[colSums(table(year, strat2)>0)>=(nyears-4)]]]
+tol4[,c("lat","lon"):=list(roundGrid(lat),roundGrid(lon))]
+setkey(tol4, lat, lon)
+tol4 <- unique(tol4)
+tol4[,plot(lon, lat, xlab="", ylab="", xlim=lon.range, ylim=lat.range, col=1+(!paste(lon,lat)%in%tol0[,paste(lon,lat)]))]
+legend("topleft", "4 missing years permitted", inset=c(-0.1, -0.12), bty="n")
+
+tol5 <- goa[strat2%in%goa[,names(colSums(table(year, strat2)>0))[colSums(table(year, strat2)>0)>=(nyears-5)]]]
+tol5[,c("lat","lon"):=list(roundGrid(lat),roundGrid(lon))]
+setkey(tol5, lat, lon)
+tol5 <- unique(tol5)
+tol5[,plot(lon, lat, xlab="", ylab="", xlim=lon.range, ylim=lat.range, col=1+(!paste(lon,lat)%in%tol0[,paste(lon,lat)]))]
+legend("topleft", "5 missing years permitted", inset=c(-0.1, -0.12), bty="n")
+
+
+toleranceChoice <- 2
+
+
+goodStrat2 <- goa[,names(colSums(table(year, strat2)>0))[colSums(table(year, strat2)>0)>=(nyears-toleranceChoice)]]
 goa <- goa[strat2%in%goodStrat2]
 goa[,stratum:=strat2]
 goa[,strat2:=NULL]
