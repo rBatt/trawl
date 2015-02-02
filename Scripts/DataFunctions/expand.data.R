f797552624f7c8806e44c608c936988742955fb6
rbatt <battrd@gmail.com>
2015-02-01 23:50:02 -0500

    pretty good expand.data
    Former-commit-id: 75ce5ea2241b01106a4b4a2f172321fc4d03ba69 [formerly ec391be35c9320844f90291568be7a5872f86033]
    Former-commit-id: efaa247a58526e822b7765273c052892d56a816c

--- Scripts/DataFunctions/expand.data.R
+++ Scripts/DataFunctions/expand.data.R
@@ -26,246 +26,158 @@
 #'@param maxOut the maximum number of allowable elements in the output array or data.table. In place to prevent early detection of a huge number of combinations that might use up a larger-than-expected amount of memory.
 
 
-expand.data <- function(comD, keyID, keyValue, arr.dim, scope=NULL, redID=NULL, redValue=NULL, fillID=arr.dim, fillValue=NA, arrayOut=FALSE, aggFun=NULL, maxOut=Inf){
-	# aggID=setdiff(keyID, union(arr.dim, scope)),
+expand.data <- function(comD, arr.dim, fillID=arr.dim, fillValue=NA, Rule, keyID=key(comD), keyValue="value", gScope=NULL, fScope, vScope, redID=NULL, redValue=NULL, arrayOut=FALSE, aggFun=NULL, maxOut=Inf){
 
-	comD = testT.sub2 # the "compact" data set
-	# keyID = c("s.reg", "stratum","year","spp","K") # columns in keyID will either define the arr.dim, the elements of the output array, or, for those elements not in either of those, will be aggreated over
-	# keyValue = c("value") # or value
-	arr.dim = c("stratum", "K", "spp")
-	# scope = list(
-	# 	c("s.reg"),
-	# 	c("s.reg","year"),
-	# 	c("s.reg")
-	# )
-	# #redID = list( # the categories to uniquely define redValue; can only include values in arr.dim, but must not include all of them
-	# 	c("spp"),
-	# 	c("stratum","year"),
-	# 	c("stratum")
-	# )
-	# redValue = list(
-	# 	c("correctSpp","taxLvl","phylum","common"), # uniquely defined by spp
-	# 	c("stemp","btemp","depth"), # uniquely defined by stratum-year
-	# 	c("lat","lon") # uniquely defined by stratum
-	# )
-	#
-	# fillID = list( # a list of character vectors, each of which is a subset of scope, and when missing levels of these factor/s are added for existing combinations of setdiff(arr.dim, fillID[[i]]), should be filled in with the corresponding value of fillValue. Note that the last element of fillID will always contain the full set in arr.dim, because the resulting array cannot be ragged.
-	# 	c("spp"),
-	# 	c("K"),
-	# 	arr.dim
-	# )
-	#
-	fillValue = c(0, NA, NA)
-	#
-	# aggFun = meanna
-	#
-	
+
 	# ==========
 	# = Checks =
 	# ==========
 	stopifnot(require(data.table))
 	stopifnot(is.data.table(comD))
-	stopifnot(class(fillValue==class(comD[,eval(parse(text=keyValue))])))
+	stopifnot(class(fillValue)==class(comD[,eval(parse(text=keyValue))]))
 	stopifnot(length(redID)==length(redValue))
 	stopifnot(length(fillID)==length(fillValue))
 	nrow.out <- comD[,prod(sapply(eval(s2c(keyID)), lu))]
 	size.out <- nrow.out * length(arr.dim)
-	if(arrOut){
+	if(arrayOut){
 		stopifnot(nrow.out<=maxOut)
 	}else{
 		stopifnot(size.out<=maxOut)
 	}
 	
 	
-	
-	
-	
-	gScope <- "s.reg" # new combinations of factor levels will be created for levels occurring w/in a given level of gScope
-	fillID <- c("spp","K") # special rules exist for extending this dimension (either non-global scope, or special fill value)
-	fScope <- list("s.reg", c("s.reg","year")) # will not create combinations of the fillID and fScope columns that did not exist in the original data; always includes global scope
-	matchCombo <- list(c("s.reg", "stratum", "K", "year")) # create all possible combinations between cbind(fillID[i],fScope[[i]]) and 
-	
-	
-	# comD[]
-	
-	
-	
-	# =========================
-	# = Save Redundant Values =
-	# =========================
-	if(!arrayOut){
-		for(r in 1:length(redID)){
-		
-		}
-	}
-
-	
-	
 	# ===================================
 	# = Aggregate over marginal keyID's =
 	# ===================================
-	IDs <- unique(c(gScope, unlist(fScope), arr.dim)) # basically the keyID of the *output* data.table
+	IDs <- unique(c(gScope, unlist(fScope), arr.dim)) # basically the keyID of the *output* data.table # TODO this definition is broken when trying to aggregate across K
 	aggID <- setdiff(keyID, IDs)
-	if(length(aggID)>0){
+	comD0 <- comD
+	if(length(aggID)>0){ # determines if it's necessary to aggregate
 		if(is.null(aggFun)){stop("arr.dim is a subset of names in keyID; must provide an aggregation function via aggFun")}
 		aggFun <- match.fun(aggFun)
 		comD <- comD[,value:=eval(s2c(keyValue))] # I overwrite comD to save memory
 		comD <- comD[,list(value=aggFun(value)), by=IDs] # aggregate step: used when not all of the values in keyID are part of arr.dim
-	}else{
+	}else{ # if it's not necessary to aggregate, still need to format a bit and drop extra columns
 		comD <- comD[,value:=eval(s2c(keyValue))]
 		comD <- comD[,eval(s2c(c(IDs,"value")))]
 	}
 	
 	
-	
-	
-	
-	
 	# ===================================
 	# = Fill out a subset of dimensions =
 	# ===================================
-	# Fill out a subset of dimensions; i.e., the elements of fillID that are not equivalent to arr.dim
-	if(length(fillID)>1){ # right now only handling case where length of fillID (and fillValue) is 1 or 2.
-		# for(i in 1:(length(fillID))) # placeholder
-		i= 2L #1L # placeholder for now
-		tfid <- fillID[i]
-		# combos.exp <- comD[,]
-		# fillBy <- setdiff(arr.dim, tfid) # setdiff(keyID, tfid)
-		# tuid <- paste0("uid",i)
-		
-		fill.within <- c(fScope[[i]])
-		# comD[,fwID:=.GRP, by=fill.within] # combinations with the filling variable that are to be maintained
-		
-		# fill.across <- c(setdiff(arr.dim, c(fScope[[i]],tfid)))
-		# comD[,faID:=.GRP, by=fill.across]
-		
-		
-		
-		setkeyv(comD, c(keyID))
-		
-		id.dt <- comD[,
-			j={
-				idset <- do.call(CJ, lapply(eval(s2c(setdiff(keyID,gScope))), unique))
-				# idset <- do.call(CJ, lapply(eval(s2c(arr.dim)), unique))
-				# print(c(nrow(idset), prod(c(lu(stratum),lu(K),lu(spp)))))
-				# print(idset[1:200], nrow=Inf)
-				# setnames(idset, names(idset), arr.dim)
-				setnames(idset, names(idset), setdiff(keyID,gScope))
-				# print(idset[,.SD[,max(K),by="stratum"]])
-				
-				setkeyv(idset, arr.dim)
-				idset#[unique(.SD[,eval(s2c(c(arr.dim)))])]
-				
-			},
-			by="s.reg" #by=c(fill.within)
-		]
-		
-		setorder(id.dt, s.reg, year, stratum, K, spp)
-		# print(id.dt[1:100], nrow=Inf)
-#
-# 		comD[,max(K),by=c("year","stratum")][,lu(V1),by="year"]#[,max(V1)]
-		id.dt[,max(K),by=c("year","stratum")][,lu((V1)),by="year"]#[,max(V1)]
-		
-		setkeyv(id.dt, keyID)
-		expD <- comD[id.dt]
-		expD[sample(1:nrow(expD), 100),]
-		
-		
-		unique(data.table(comD[,list(year, K)], key=c("year","K")))
-		
-		orig.year.K <- unique(data.table(comD[,list(year, K)], key=c("year","K")))
-		
-		setkeyv(expD, key(orig.year.K))
-		expD <- expD[orig.year.K]
-		
-		orig.stratum.year.K <- unique(data.table(comD[,list(stratum, year, K)], key=c("stratum","year","K")))
-		
-		setkeyv(expD, key(orig.stratum.year.K))
-		expD[orig.stratum.year.K, fill.spp:=0]
-		expD <- expD[is.na(value), value:=fill.spp]
-		expD[,fill.spp:=NULL]
-		
-		# new.combos <- c(fill.across, tfid)
-		
-		# setkeyv(comD, new.combos)
-		
-		
-		
-		id.dt <- comD[,
-			j={
-				idset <- do.call(CJ, lapply(eval(s2c(c("fwID", tfid))), unique))
-				setnames(idset, names(idset), c("fwID", tfid))
-				# print(idset)
-				setkey(idset, fwID)
-				idset[unique(.SD[,eval(s2c(c("fwID", "faID", fillBy)))])]
-			},
-			by=c(fScope[[i]]) # fwID probably isn't needed b/c this is here
-		]
-		setkeyv(id.dt, cols=c(gScope, tfid, fillBy)) # would just do arr.dim but it has to be in this order (i.e., with fillID[[1]] 1st)
-		setkeyv(comD, cols=c(gScope, tfid, fillBy))
-		fill.gaps <- comD[id.dt, which=TRUE, allow.cartesian=TRUE]
-		id.dt[,value:=comD[fill.gaps,.(value)]]
-		id.dt[is.na(fill.gaps),value:=fillValue[i]]
-		
-		# Cleanup, prepare
-		id.dt[,eval(tuid):=NULL]
-		setkeyv(id.dt, cols=c(arr.dim))
-	}
-
-
-	# ==============================
-	# = Fill in remainder of array =
-	# ==============================
-	# Always the final step of filling out array: Fill in the rest of the elements of the array w/ NA's
-	tfid <- fillID[[length(fillID)]]
-	fillBy <- setdiff(arr.dim, tfid)
-	tuid <- paste0("uid","Fin")
+	# Fill out a subset of dimensions; i.e., the elements of fillID that are not equivalent to arr.dim		
+	setkeyv(comD, c(keyID))
 	
-	id.dt[,eval(tuid):=.GRP, by=scope]
-	setkey(id.dt, eval(tuid))
-	
-	expD <- id.dt[,
+	id.dt <- comD[,
 		j={
-			idset <- do.call(CJ, lapply(eval(s2c(c(tuid, tfid))), unique))
-			setnames(idset, names(idset), c(tuid, tfid))
-			setkeyv(idset, cols=c(tuid))
-			idset[unique(.SD[,eval(s2c(c(tuid, fillBy)))])]
+			idset <- do.call(CJ, lapply(eval(s2c(setdiff(keyID,gScope))), unique))
+			setnames(idset, names(idset), setdiff(keyID,gScope))
+			idset
 		},
-		by=scope
+		by=c(gScope)
 	]
+	# setorder(id.dt, s.reg, year, stratum, K, spp)
+	# id.dt[,max(K),by=c("year","stratum")][,lu((V1)),by="year"]
 	
-	setkeyv(expD, cols=c(scope, tfid)) # would just do arr.dim but it has to be in this order (i.e., with fillID[[1]] 1st)
-	setkeyv(id.dt, cols=c(scope, tfid))
-	
-	fill.gaps <- id.dt[id.dt, which=TRUE, allow.cartesian=TRUE]
-	expD[,value:=id.dt[fill.gaps,.(value)]]
-	expD[is.na(fill.gaps),value:=fillValue[i]]
-	
-	# Cleanup, prepare
-	expD[,eval(tuid):=NULL]
-	setkeyv(expD, cols=c(arr.dim))
-	
-	
-	# expD <- id.dt[do.call(CJ, lapply(eval(s2c(arr.dim)), unique)), allow.cartesian=TRUE] # TODO allow for the fill value to be something other than NA's; would require creating object like fill.gaps
-
-	
-	# ===================
-	# = Return if array =
-	# ===================
-	if(arrayOut){
-		# scope = c("year", "s.reg")
-# 		combn.out <- expD[,do.call(CJ, lapply(eval(s2c(scope)), unique))]
-# 		nlist <- do.call(CJ, lapply(eval(s2c(c(tuid, tfid))), unique))
-#
-# 		array.list <- list()
+	setkeyv(id.dt, keyID)
+	expD <- comD[id.dt]
+	# expD[sample(1:nrow(expD), 100),]
+	if(length(fillID)>1){
+		for(i in 1:length(fillID)){
+			t.fID <- fillID[i]
+			if(Rule[i]=="scope"){
+				t.cols <- c(fScope[[i]], t.fID)
+				orig <- unique(data.table(comD[,eval(s2c(t.cols))], key=c(t.cols))) # the original combinations of IDs
 		
-		dim.names <- lapply(expD[,eval(s2c(arr.dim))], unique)
-		sapply(dim.names, length)
-		array(expD[,value], dim=sapply(dim.names, length), dimnames=dim.names)
+				setkeyv(expD, key(orig))
+				expD <- expD[orig]
+			}
+			
+			if(Rule[i]=="value"){
+				t.cols <- c(vScope[[i]])
+				orig <- unique(data.table(comD[,eval(s2c(t.cols))], key=c(t.cols))) # the original combinations of IDs
+				
+				setkeyv(expD, key(orig))
+				expD[orig, t.fill:=fillValue[i]] # new column w/ NA or, if orig IDs found, fillValue
+				expD <- expD[is.na(value), value:=t.fill]
+				expD[,t.fill:=NULL]
+			}
+		}
 	}
 	
-	
+	# ==============================
+	# = Return array or data.table =
+	# ==============================
+	if(arrayOut){ # return if array
+		
+		outScope <- union(unlist(gScope), unlist(fScope)) # the output scope determiens the number of arrays that need to be formed
+		outsize <- nrow(unique(data.table(expD[,eval(s2c(outScope))], key=c(outScope)))) # the number of arrays
+		array.list <- vector("list", outsize) # preallocate data array
+		array.key <- vector("list", outsize) # the list of arrays can be long and hard to navigate; this key will supply the outScope combinations present in each element of the array.list output list
+		
+		invisible(expD[, # within the j of this data.table, build each element of the output array list
+			j={
+				dim.names <- lapply(.SD[,eval(s2c(arr.dim))], unique)
+				array.list[[.GRP]] <<- array(.SD[,value], dim=sapply(dim.names, length), dimnames=dim.names)
+				array.key[[.GRP]] <<- c(unlist(.BY),.GRP) # grab the names of the by= groups, add them to the output key
+			},
+			by=c(outScope)
+		])
+		
+		# lapply(array.list, dim)
+		
+		array.key <- data.table(matrix(unlist(array.key), ncol=length(outScope)+1, byrow=TRUE)) # build the key to the output array
+		setnames(array.key, names(array.key), c(outScope, "num")) # key the key
+		
+		return(list(array.list=array.list, array.key=array.key)) # return the output array and its key (stops function)
+		
+	}else{ # end first half of if(arrayOut), begin alternative
+	# ==================================================================
+	# = If not an array, return formatted data table with "red" values =
+	# ================================================================== 
+		if(!is.null(redID)){ # if there are redID's that should be added back in ...
+			for(i in 1:length(redID)){ # for each redundant id/ value ...
+				redNames <- c(redID[[i]], redValue[[i]]) # get the names of the redundant ID (the value chosen to represent others), and the redundant values
+				setkey(expD, spp) # set the key of expD to be 
+				expD <- unique(data.table(comD0[,eval(s2c(redNames))], key=c(redNames)))[expD,][,eval(s2c(c(names(expD),redValue[[i]])))]
+			} # end loop through redID
+		} # end redID if
+		return(expD)
+	} # end if(){}else{} for arrayOut
+} # end expand.data()
 
-}
 
 
+
+
+msom.array <- expand.data(
+	comD = testT.sub2,
+	arr.dim = c("stratum", "K", "spp"), # should uniquely define the keyValue when combined with fScope
+	fillID=c("spp","K"),
+	fillValue=c(0,NA), # values to fill with, for a fillID
+	Rule=c("value", "scope"), # does fillID use a non-NA fill value, or does it have restricted (more specific than "global") scope?
+	keyID=key(comD), # column names whose values uniquely identify rows
+	keyValue="value", # the column whose values would fill the array 
+	gScope="s.reg", # global scope
+	fScope=list("s.reg", c("s.reg","year")), # 
+	vScope=list(c("s.reg","stratum","year","K"), NULL),
+	redID=list(c("spp")), redValue=list(c("correctSpp","taxLvl","phylum","common")),
+	arrayOut=TRUE, aggFun=meanna, maxOut=Inf
+)
+
+
+array.filled <- expand.data( # this test the aggregate functionality, data.table output, non-NA fill, 1 fillID
+	comD = testT.sub,
+	arr.dim = c("stratum", "year", "spp"), # should uniquely define the keyValue when combined with fScope
+	fillID=c("spp"),
+	fillValue=c(0), # values to fill with, for a fillID
+	Rule=c("value"), # does fillID use a non-NA fill value, or does it have restricted (more specific than "global") scope?
+	keyID=c("s.reg", "stratum","year","spp"), # column names whose values uniquely identify rows in the input
+	keyValue="value", # the column whose values would fill the array 
+	gScope="s.reg", # global scope
+	fScope=list("s.reg"), # 
+	vScope=list(c("s.reg","stratum","year","spp")),
+	redID=list(c("spp")), redValue=list(c("correctSpp","taxLvl","phylum","common")),
+	arrayOut=FALSE, aggFun=meanna, maxOut=Inf
+)
