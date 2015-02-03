49b6b2ac48a3725b9646f8e7c42685faa932dbd6
rbatt <battrd@gmail.com>
2015-02-03 12:34:20 -0500

    fixed combine.trawl, rm use of expand.data
    put expand.data in its own location
    
    
    Former-commit-id: 503e356194cd8d4a0852856b162504d46b6d534b [formerly 9bce7cc312efc1629b730f11f8c103e378ee4a1c]
    Former-commit-id: 032d158547ca5ee35b86c5d18569fc98615792ec

--- Scripts/Data/combine.trawl.R
+++ Scripts/Data/combine.trawl.R
@@ -402,94 +402,9 @@ if(delOldTrawl){
 	rm(list="trawl3")
 }
 
-
-# ===============================================
-# = Add missing species, padding w/ 0's or NA's =
-# ===============================================
-# ============================
-# = Add missing combinations =
-# ============================
-# Make sure all of a region's strata are present in all years
-# Make sure all of a region's species are present in each stratum in each year
-# If the s.reg-stratum-year was present, but the s.reg-stratum-year-spp was not, fill in w/ 0, NA otherwise
-# Aggregate among "K" replicate hauls (substrata)
-setkey(trawl2, s.reg, year, stratum, K, spp)
-trawl <- expand.data(
-	comD = trawl2,
-	arr.dim = c("stratum", "year", "spp"), # should uniquely define the keyValue ONLY WHEN combined with fScope (don't accidentally include the gScope value here)
-	fillID=c("spp"),
-	fillValue=c(0), # values to fill with, for a fillID
-	Rule=c("value"), # does fillID use a non-NA fill value, or does it have restricted (more specific than "global") scope?
-	keyID=c("s.reg", "stratum","year","spp", "K"), # column names whose values uniquely identify rows in the input
-	keyValue="wtcpue", # the column whose values would fill the array
-	gScope="s.reg", # global scope
-	fScope=list("s.reg"), #
-	vScope=list(c("s.reg","stratum","year")),
-	redID=list(
-		c("spp"),
-		c("s.reg","year","stratum","spp")
-	), 
-	redValue=list(
-		c("correctSpp","taxLvl","phylum","common"),
-		c("lat", "lon", "depth", "stemp", "btemp")
-	),
-	arrayOut=FALSE, aggFun=meanna, maxOut=Inf
-)
-	#
-	#
-	#
-	# comD = trawl2
-	# arr.dim = c("stratum", "year", "spp") # should uniquely define the keyValue ONLY WHEN combined with fScope (don't accidentally include the gScope value here)
-	# fillID=c("spp")
-	# fillValue=c(0) # values to fill with, for a fillID
-	# Rule=c("value") # does fillID use a non-NA fill value, or does it have restricted (more specific than "global") scope?
-	# keyID=c("s.reg", "stratum","year","spp", "K") # column names whose values uniquely identify rows in the input
-	# keyValue="wtcpue" # the column whose values would fill the array
-	# gScope="s.reg" # global scope
-	# fScope=list("s.reg") #
-	# vScope=list(c("s.reg","stratum","year"))
-	# redID=list(
-	# 	c("spp"),
-	# 	c("s.reg","year","stratum","spp")
-	# )
-	# redValue=list(
-	# 	c("correctSpp","taxLvl","phylum","common"),
-	# 	c("lat", "lon", "depth", "stemp", "btemp")
-	# )
-	# arrayOut=FALSE
-	# aggFun=meanna
-	# maxOut=Inf
-
-
-
-# ==================================
-# = Example of how to prepare MSOM =
-# ==================================
-setkey(trawl2, s.reg, year, stratum, K, spp)
-msom.dat <- expand.data(
-	comD = trawl2,
-	arr.dim = c("stratum", "K", "spp"), # should uniquely define the keyValue ONLY WHEN combined with fScope (don't accidentally include the gScope value here)
-	fillID=c("spp","K"),
-	fillValue=c(0,NA), # values to fill with, for a fillID
-	Rule=c("value","scope"), # does fillID use a non-NA fill value, or does it have restricted (more specific than "global") scope?
-	keyID=c("s.reg", "stratum","year","spp", "K"), # column names whose values uniquely identify rows in the input
-	keyValue="wtcpue", # the column whose values would fill the array
-	gScope="s.reg", # global scope
-	fScope=list("s.reg", c("s.reg","year")), #
-	vScope=list(c("s.reg","stratum","year"), NULL),
-	redID=NULL, 
-	redValue=NULL,
-	arrayOut=TRUE, aggFun=meanna, maxOut=Inf
-)
-
-
-# ========
-# = Save =
-# ========
-setkey(trawl, s.reg, spp, year, stratum)
-save(trawl, file="/Users/Battrd/Documents/School&Work/pinskyPost/trawl/Data/trawl.RData")
-
+# ======================
+# = Save the raw trawl =
+# ======================
 setkey(trawl2, s.reg, year, stratum, K, phylum, spp)
 save(trawl2, file="/Users/Battrd/Documents/School&Work/pinskyPost/trawl/Data/trawl2.RData")
 
-save(msom.dat, file="/Users/Battrd/Documents/School&Work/pinskyPost/trawl/Data/msom.dat.RData")
