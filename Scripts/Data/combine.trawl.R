dfd9907733ae9207977b4c7a37e9ced4b2a45439
rbatt <battrd@gmail.com>
2015-02-04 16:30:35 -0500

    put functions 4 cleaning spp names (regex) in own file
    moved from combine.trawl.R to cleanSppNames.R
    
    so they can be sourced
    
    
    Former-commit-id: 44cdcffcadae58e706af15b230ad3e3759262713 [formerly 2151a746d493864c9846deaaec3192b014bb582e]
    Former-commit-id: 6a4342b7892a3906cc5767ada87d3a4e3476a992
    Former-commit-id: 409af56b4bd40e6f20b2425cc87c410a84faea47

--- Scripts/Data/combine.trawl.R
+++ Scripts/Data/combine.trawl.R
@@ -73,31 +73,6 @@ if(delOldTrawl){
 }
 
 
-# ====================================
-# = Functions for cleaning spp names =
-# ====================================
-fixCase <- function(x){
-	s <- paste(toupper(substring(x, 1, 1)), substring(x, 2), sep="")
-	paste(substring(s, 1, 1), tolower(substring(s, 2)), sep="")
-}
-
-cullExSpace <- function(x){
-	gsub("\\s+", " ", x)
-}
-
-cullSp <- function(x){
-	gsub("\\s(s[p]{1,2}|unid)\\..*", "", x)
-}
-
-cullParen <- function(x){
-	gsub("\\s?\\(.*\\)", "", x)
-}
-
-is.species <- function(x){
-	sapply(strsplit(x, " "), length) >= 2
-}
-
-
 # =============================
 # = Clean and reaggregate spp =
 # =============================
