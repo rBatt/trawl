98419ecc7aa0f371aef8144d6f7851e38ee2d5a8
rbatt <battrd@gmail.com>
2015-02-04 16:27:36 -0500

    need K in vScope for msom.dat, exclude non-spp
    not that K isn't needed for trawl, because in trawl we average over the replicates
    
    also, in both trawl and msom.dat creation, removed non-species (correctSpp, has a common, and is.species() says TRUE)
    
    
    Former-commit-id: 3f9baf354ecfc958bf1ddc3b648b04b8af6a3196 [formerly 66aac25cfba088b4500e827b9f396c95ad3454df]
    Former-commit-id: a835384a01d24b717ec9f75562afa9325a0f5ca0

--- Scripts/Data/format.trawl.R
+++ Scripts/Data/format.trawl.R
@@ -28,7 +28,13 @@ invisible(sapply(paste(data.location, list.files(data.location), sep="/"), sourc
 # =======================
 # = Load trawl2 Dataset =
 # =======================
-load("/Users/Battrd/Documents/School&Work/pinskyPost/trawl/Data/trawl2.RData")
+# load("/Users/Battrd/Documents/School&Work/pinskyPost/trawl/Data/trawl2.RData")
+load("./trawl/Data/trawl2.RData")
+
+# ======================
+# = Remove unknown spp =
+# ======================
+trawl2.veri <- trawl2[(correctSpp)&!is.na(common)&is.species(spp),]
 
 
 # ============================
@@ -38,9 +44,9 @@ load("/Users/Battrd/Documents/School&Work/pinskyPost/trawl/Data/trawl2.RData")
 # Make sure all of a region's species are present in each stratum in each year
 # If the s.reg-stratum-year was present, but the s.reg-stratum-year-spp was not, fill in w/ 0, NA otherwise
 # Aggregate among "K" replicate hauls (substrata)
-setkey(trawl2, s.reg, year, stratum, K, spp)
+setkey(trawl2.veri, s.reg, year, stratum, K, spp)
 trawl <- expand.data(
-	comD = copy(trawl2),
+	comD = copy(trawl2.veri),
 	arr.dim = c("stratum", "year", "spp"), # should uniquely define the keyValue ONLY WHEN combined with fScope (don't accidentally include the gScope value here)
 	fillID=c("spp"),
 	fillValue=c(0), # values to fill with, for a fillID
@@ -49,7 +55,7 @@ trawl <- expand.data(
 	keyValue="wtcpue", # the column whose values would fill the array
 	gScope="s.reg", # global scope
 	fScope=list("s.reg"), #
-	vScope=list(c("s.reg","stratum","year")),
+	vScope=list(c("s.reg","stratum", "year")),
 	redID=list(
 		c("spp"),
 		c("s.reg","year","stratum","spp")
@@ -102,7 +108,7 @@ rm(list="trawl")
 # ==================================
 setkey(trawl2, s.reg, year, stratum, K, spp)
 msom.dat <- expand.data(
-	comD = copy(trawl2),
+	comD = copy(trawl2.veri),
 	arr.dim = c("stratum", "K", "spp"), # should uniquely define the keyValue ONLY WHEN combined with fScope (don't accidentally include the gScope value here)
 	fillID=c("spp","K"),
 	fillValue=c(0,NA), # values to fill with, for a fillID
@@ -111,11 +117,22 @@ msom.dat <- expand.data(
 	keyValue="wtcpue", # the column whose values would fill the array
 	gScope="s.reg", # global scope
 	fScope=list("s.reg", c("s.reg","year")), #
-	vScope=list(c("s.reg","stratum","year"), NULL),
+	vScope=list(c("s.reg","stratum","K","year"), NULL),
 	redID=NULL, 
 	redValue=NULL,
 	arrayOut=TRUE, aggFun=meanna, maxOut=Inf
 )
+
+# apply(msom.dat[[1]][[1]], c(1,2), sumna)
+# apply(msom.dat[[1]][[2]], c(1,2), sumna)
+#
+# test.no0 <- function(x){x2 <- apply(x, c(1,2), sumna); sum(x2[!is.na(x2)]==0)}
+# sapply(msom.dat[[1]], test.no0) # indicates that 223 was a 0
+# any(apply(msom.dat[[1]][[223]], c(1,2), sumna)==0)
+# bad.eg <- which(apply(msom.dat[[1]][[223]], c(1,2), sumna)==0, arr.ind=TRUE)
+# trawl2.veri[s.reg=="shelf"&year=="1979"&stratum==rownames(bad.eg)&K==bad.eg[2]] # it actually IS a 0!!!
+
+
 
 # ========
 # = Save =
@@ -123,7 +140,8 @@ msom.dat <- expand.data(
 # save(msom.dat, file="/Users/Battrd/Documents/School&Work/pinskyPost/trawl/Data/msom.dat.RData")
 save(msom.dat, file="./trawl/Data/msom.dat.RData")
 
-#
+
+
 # trawl.test <- trawl2[s.reg%in%c("gmex","ai","neus") & year%in%c("1983","1986")]
 # comD = copy(trawl.test)
 # arr.dim = c("stratum", "K", "spp") # should uniquely define the keyValue ONLY WHEN combined with fScope (don't accidentally include the gScope value here)
@@ -134,10 +152,9 @@ save(msom.dat, file="./trawl/Data/msom.dat.RData")
 # keyValue="wtcpue" # the column whose values would fill the array
 # gScope="s.reg" # global scope
 # fScope=list("s.reg", c("s.reg","year"))
-# vScope=list(c("s.reg","stratum","year"), NULL)
+# vScope=list(c("s.reg","stratum", "K", "year"), NULL)
 # redID=NULL
 # redValue=NULL
 # arrayOut=TRUE
 # aggFun=meanna
 # maxOut=Inf
-#
