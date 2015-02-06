d9292585b22bd4f7a34ca0dcc388eca3b9854597
rbatt <battrd@gmail.com>
2015-02-06 16:39:14 -0500

    implement thinning in basic richness
    Former-commit-id: 7c43c53df2a88d304f8c8eef4fa3e65237a9b094 [formerly 22a5ce6007ac3a43049a424d4e3e775d79bb2f9c]
    Former-commit-id: f400e6f119d420afac78327f7953bc2e3a3869ad

--- Scripts/Analysis/diversity/richness.basic.R
+++ Scripts/Analysis/diversity/richness.basic.R
@@ -2,10 +2,10 @@
 # ================
 # = JAGS Options =
 # ================
-nChains <- 5
-nIter <- 5E3
-n0s <- 100
-
+nChains <- 3
+nIter <- 4E3
+n0s <- 5E2
+nThin <- 10 # max(1, floor((nIter - floor(nIter/2)) / 1000))
 
 # =================
 # = Load packages =
@@ -47,6 +47,7 @@ invisible(sapply(paste(plot.location, list.files(plot.location), sep="/"), sourc
 # = Load MSOM Data set =
 # ======================
 load("./trawl/Data/msom.dat.RData")
+load("./trawl/Data/trawl2.RData") # needed so that I know where the original NA's are; not in format.trawl b/c this will need to be handled differently for the different richness models, so this is richness.basic specific
 
 
 # =====================================
@@ -63,6 +64,58 @@ if(Sys.info()["sysname"]=="Windows"){
 }
 
 
+# ======================================
+# = Change original data's NA's to 1's =
+# ======================================
+change.na.1 <- trawl2[is.na(wtcpue), list(s.reg=s.reg, year=year, stratum=stratum, K=K, spp=spp)]
+setkey(msom.dat[[2]], s.reg, year)
+md.num <- msom.dat[[2]][change.na.1[,list(s.reg,as.character(year))]][,as.numeric(num)]
+for(i in 1:length(md.num)){
+	t.c <- change.na.1[i,c(stratum=stratum,K=K,spp=spp)]
+	tryCatch(
+		{
+			if(!is.na(msom.dat[[1]][[md.num[i]]][t.c[1],t.c[2],t.c[3]])){
+				print("already fixed!")
+			}else{
+				msom.dat[[1]][[md.num[i]]][t.c[1],t.c[2],t.c[3]] <- 1
+			}
+		
+		}, 
+		error=function(cond){print(paste0(t.c,collapse=" "))} # sometimes the subset doesn't exist b/c that particular species from trawl2 was removed before the creation of msom.dat b/c it wasn't correctSpp, or it didn't have a common name, or is.species() was F, etc. For example, spp == "Gastropoda" or spp=="Trash species in catch" will not be included here. spp=="Ophichthus ocellatus" wasn't included because it doesn't have a common name (but see issue #26) 
+	)
+}
+
+
+# =======================================
+# = Function to remove unsampled strata =
+# =======================================
+prep.basic <- function(t.dat){	
+	keep.strat <- apply(t.dat, 1, function(x)!all(is.na(x))) # which strata were never sampled?
+	t.dat <- t.dat[keep.strat,,] # remove strata that were never sampled
+
+	# If there are NA's 
+	orig.na.TF <- apply(t.dat, c(1,3), function(x){all(is.na(x))})# sometimes there were NA's in the original data – this implies the species was observed, but perhaps effort wasn't recorded (/ by 0), or some other reason why wtcpue couldn't be recorded.
+	if(any(orig.na.TF)){
+		stop("A species was NA for all reps in a stratum. Please check the following: 1) replace original NA's with 1, 2) if other species have non-NA for this s.reg-year-stratum-K, then this species should have been changed to 0 in expand.data, 3) if all species are NA for all reps in this s.reg-year-stratum, then the stratum should have been dropped at the start of this function")
+		# print("found one!")
+		# orig.na.ind <- which(orig.na.TF, arr.ind=TRUE) # not easy to use orig.na.TF as subset b/c it doesn't reference all 3 dimensions
+		# orig.na.ind.c <- c(orig.na[,1], rep(1,nrow(orig.na)), orig.na[,2]) # we'll just change the first column (K) to not-NA
+		# orig.na.2.1 <- matrix(orig.na.ind.c, ncol=3, dimnames=list(rownames(orig.na),c("dim1","dim2","dim3")))
+		# t.dat[orig.na.2.1] <- 1 # all non-zero positive values will be turned into 1 in the occurrence model
+	}
+	
+
+	return(t.dat)
+
+}
+
+
+# ===================================
+# = Prep Data for Bayesian Richness =
+# ===================================
+t.dat <- lapply(msom.dat[[1]], prep.basic)
+
+
 # ==============================
 # = Function to combine output =
 # ==============================
@@ -76,23 +129,17 @@ comb <- function(...){
 # =========================
 # = Run Bayesian Richness =
 # =========================
-t.dat <- msom.dat[[1]][[1]] # subset to data for first run
-keep.strat <- apply(t.dat, 1, function(x)!all(is.na(x))) # which strata were never sampled?
-t.dat <- t.dat[keep.strat,,] # remove strata that were never sampled
-first.out <- rich.basic(t.dat, nzeroes=n0s, nChains=nChains, nIter=nIter) # run the model for the first subset
+# Run first Bayesian richness (done separately b/c combine function in foreach needs starting output)
+first.out <- rich.basic(t.dat[[1]], nzeroes=n0s, nChains=nChains, nIter=nIter, nThin=nThin) # run the model for the first subset
 
-richness.basic.out <- foreach(i=(2:(length(msom.dat[[1]])-1)), .combine=comb, .init=first.out, .multicombine=TRUE) %dopar%{ # run all other subsets in parallel
-	t.dat <- msom.dat[[1]][[i]] # subset to current iteration
-	keep.strat <- apply(t.dat, 1, function(x)!all(is.na(x))) # figure out which strata to toss out
-	t.dat <- t.dat[keep.strat,,] # only keep observed strata
-	rich.basic(t.dat, nzeroes=n0s, nChains=nChains, nIter=nIter) # do analysis for this subset
+# Run all other Bayesian richness in parallel
+richness.basic.out <- foreach(i=(2:(length(t.dat)-1)), .combine=comb, .init=first.out, .multicombine=TRUE) %dopar%{ # run all other subsets in parallel
+	rich.basic(t.dat[[i]], nzeroes=n0s, nChains=nChains, nIter=nIter, nThin=nThin) # do analysis for this subset
 }
 
 
-# testing[[10]][[1]] # is the normal bugs output for the first run
-# testing[[10]][[2]] # is the normal bugs output for the second run
-# testing[[10]][[3]] # is the normal bugs output for the third run
-
-
+# ===============
+# = Save Output =
+# ===============
 save(richness.basic.out, file="./trawl/Results/Richness/richness.basic.out.RData")
 
