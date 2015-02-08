80dd47c474640f55840bcaa3ce1ffc2ba6b4b899
rbatt <battrd@gmail.com>
2015-02-08 16:42:00 -0500

    add an alpha value to a color
    Former-commit-id: d85290c6ac2ec224f4b52297ea1f735c2d218c0a [formerly 41d7f48f1912b5194d9d469ae4150880f983f1f6]
    Former-commit-id: 6d4b58b7a10cca1cfd645a1abed469f805a07644

---
+++ Scripts/PlotFunctions/add.alpha.R
@@ -0,0 +1,7 @@
+# http://www.magesblog.com/2013/04/how-to-change-alpha-value-of-colours-in.html
+add.alpha <- function(col, alpha=1){
+	if(missing(col)){
+		stop("Please provide a vector of colours.")
+	}
+	apply(sapply(col, col2rgb)/255, 2, function(x)rgb(x[1],x[2],x[3],alpha=alpha))
+}
