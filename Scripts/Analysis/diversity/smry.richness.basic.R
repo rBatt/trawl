fa235c1a31d90428f90c14484eaf6d4d33728a57
rbatt <battrd@gmail.com>
2015-02-07 19:26:09 -0500

    do missed run, fix crazy out;
    Former-commit-id: 963bc9c8824b2958c2099d1f86e20037150db472 [formerly de8ae74112a9f0161fa689e9fb435e4dcd0bb456]
    Former-commit-id: 4bcdf7ff8d8b2bca1d3644ca2a68ff8cf0e083f0

--- Scripts/Analysis/diversity/smry.richness.basic.R
+++ Scripts/Analysis/diversity/smry.richness.basic.R
@@ -1,10 +1,39 @@
 
+
+# ===============================
+# = Guess appropriate directory =
+# ===============================
+if(Sys.info()["sysname"]=="Linux"){
+	setwd("~/Documents/School&Work/pinskyPost")
+}else{
+	setwd("~/Documents/School&Work/pinskyPost")
+}
+
+
+# ================
+# = Load Results =
+# ================
+load("~./trawl/Results/Richness/richness.basic.out.RData")
+load("~./trawl/Results/Richness/last.out.RData")
+
+
 # =====================================
 # = Post-processing of richness.basic =
 # =====================================
-
-trueN <- first.out$N
-obsN <- sapply(t.dat, function(x)dim(x)[3])
+# NOTE: I screwed up the combine function for the run of richness.basic.R on 06-Feb-2015. I also forgot to run the last region-year combo. So for the first 274 I had to manually edit their combination, and only bothered with the sims.list (everything else is a summary of that anyway). Then when I noticed that I didn't run #275, I ran it by itself, called it last.out, and am adding it on here. I'm saving this as rbo.RData. So that richness.basic.out.RData is super messed up!
 
 
-str(first.out$Z)
+rbo.11 <- richness.basic.out[[11]] # UGH
+rbo.11.1 <- unlist(rbo.11[[1]], F)
+
+# I got last out manually
+rbo <- c(rbo.11.1, unlist(rbo.11[-1], F), last.out[[11]])
+# save(rbo, file="./trawl/Results/Richness/rbo.RData", compress="xz")
+
+
+
+
+
+# test <- do.call(Map, c(c, rbo)) # DON'T DO THIS – IT'LL CRASH OR TAKE FOREVER
+
+
