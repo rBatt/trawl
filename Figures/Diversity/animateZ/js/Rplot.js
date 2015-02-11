f6342889c1d6e7dc9d0cdd8bdde84332958b0836
rbatt <battrd@gmail.com>
2015-02-10 21:36:02 -0500

    movies of richness in space and time

---
+++ Figures/Diversity/animateZ/js/Rplot.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#Rplot').scianimator({
+	    'images': ['zpngs/Rplot1.png', 'zpngs/Rplot2.png', 'zpngs/Rplot3.png', 'zpngs/Rplot4.png', 'zpngs/Rplot5.png', 'zpngs/Rplot6.png', 'zpngs/Rplot7.png', 'zpngs/Rplot8.png', 'zpngs/Rplot9.png', 'zpngs/Rplot10.png', 'zpngs/Rplot11.png', 'zpngs/Rplot12.png', 'zpngs/Rplot13.png', 'zpngs/Rplot14.png', 'zpngs/Rplot15.png', 'zpngs/Rplot16.png', 'zpngs/Rplot17.png', 'zpngs/Rplot18.png', 'zpngs/Rplot19.png', 'zpngs/Rplot20.png', 'zpngs/Rplot21.png', 'zpngs/Rplot22.png', 'zpngs/Rplot23.png', 'zpngs/Rplot24.png', 'zpngs/Rplot25.png', 'zpngs/Rplot26.png', 'zpngs/Rplot27.png', 'zpngs/Rplot28.png', 'zpngs/Rplot29.png', 'zpngs/Rplot30.png', 'zpngs/Rplot31.png', 'zpngs/Rplot32.png', 'zpngs/Rplot33.png', 'zpngs/Rplot34.png', 'zpngs/Rplot35.png', 'zpngs/Rplot36.png', 'zpngs/Rplot37.png', 'zpngs/Rplot38.png', 'zpngs/Rplot39.png', 'zpngs/Rplot40.png', 'zpngs/Rplot41.png', 'zpngs/Rplot42.png', 'zpngs/Rplot43.png', 'zpngs/Rplot44.png', 'zpngs/Rplot45.png', 'zpngs/Rplot46.png'],
+	    'width': 1175.6067594106,
+	    'delay': 1000,
+	    'loopMode': 'loop'
+	});
+	$('#Rplot').scianimator('play');
+    });
+})(jQuery);
