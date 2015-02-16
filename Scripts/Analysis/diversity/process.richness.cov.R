87fefcef2378e68382038245e0bf30633fa702cc
rbatt <battrd@gmail.com>
2015-02-15 22:03:45 -0500

    handle shelf 2011, aggregate spp time, combine WC
    the scotian shelf in 2011 didn't have any bottom temperature data, so those parameters had to be omitted from the model. Need to handle this when combining output, else error.
    
    Create objects that aggregate over spp and time in rco – many of the RCO columns were constant for a region, year, or species, leading to many duplicates. These aggregations are convenient for later calculations.
    
    Combine the west coast triennial and annual surveys by dropping annual in the overlapping year (2003), and renaming the region to "WC"

--- Scripts/Analysis/diversity/process.richness.cov.R
+++ Scripts/Analysis/diversity/process.richness.cov.R
@@ -73,6 +73,16 @@ for(i in 1:length(all.out)){
 	idNum <- as.numeric(idKey[nameID==smryNameID[i],num])
 	
 	t.ao <- all.out[[i]]
+	if(i == 255){
+		t.ao$mean$a1 <- NA
+		t.ao$median$a1 <- NA
+		t.ao$sd$a1 <- NA
+		
+		t.ao$mean$a2 <- NA
+		t.ao$median$a2 <- NA
+		t.ao$sd$a2 <- NA
+
+	}
 
 	idStrats <- dimnames(prepd.cov.dat[[idNum]])[1][[1]]
 	idSpp <- dimnames(prepd.cov.dat[[idNum]])[3][[1]]
@@ -84,9 +94,9 @@ for(i in 1:length(all.out)){
 	
 	all.out[[i]]$ID <- into.out
 	
-	n.spp <- length(t.ao$mean$a1)
+	n.spp <- length(t.ao$mean$a3)
 	n0s <- n.spp - length(idSpp)
-	spp.tot <- length(t.ao$mean$a1)
+	spp.tot <- length(t.ao$mean$a3)
 	all.spp <- c(idSpp, paste0("unobs",1:n0s))
 	
 	nS <- length(idStrats)
@@ -127,7 +137,7 @@ for(i in 1:length(all.out)){
 			a2=rep(t.ao$median$a2, each=nS),
 			a3=rep(t.ao$median$a3, each=nS),
 			a4=rep(t.ao$median$a4, each=nS)
-			
+		
 		))
 	}
 	
@@ -135,5 +145,47 @@ for(i in 1:length(all.out)){
 
 ao.rco <- all.out
 
+rco[,year:=as.numeric(as.character(year))]
+
+rco <- rco[s.reg!="wcann" | (s.reg=="wcann" & year > 2003)]
+rco[s.reg=="wcann" | s.reg=="wctri", s.reg:="wc"]
+
+
+
+# ============================
+# = Aggregate RCO across spp =
+# ============================
+# Leaving site and year
+rco.sy <- copy(rco)
+rco.sy[,c("spp","Z", "u.a0","a1","a2","a3","a4"):=NULL] # these values aren't useful w/o species distinction
+setkey(rco.sy, s.reg, year, stratum)
+rco.sy <- unique(rco.sy)
+
+
+# =====================================
+# = Aggregate RCO across spp and year =
+# =====================================
+qSlope <- function(x, y){if(length(x)<2){return(NA_real_)}else{lm(y~x)$coef[2]}}
+rco.s <- copy(rco.sy)
+rco.s[,nameID:=NULL]
+rco.s <- rco.s[,
+	list(
+		mu.btemp=mean(btemp, na.rm=TRUE),
+		slope.btemp=qSlope(year,btemp),
+		mu.depth=mean(depth, na.rm=TRUE),
+		slope.depth=qSlope(year, depth),
+		mu.N=mean(N, na.rm=TRUE),
+		slope.N=qSlope(year, N),
+		mu.Nsite=mean(Nsite, na.rm=TRUE),
+		slope.Nsite=qSlope(year, Nsite)
+	),
+	by=c("s.reg","stratum","lon","lat")
+]
+
+
+
 save(rco, file="./trawl/Results/Richness/rco.RData")
 save(ao.rco, file="./trawl/Results/Richness/ao.rco.RData")
+save(rco.s, file="./trawl/Results/Richness/rco.s.RData")
+save(rco.sy, file="./trawl/Results/Richness/rco.sy.RData")
+
