6da45a4e36cc3d80b55dc6124d8b9ebfe6b97a83
rbatt <battrd@gmail.com>
2015-03-10 13:42:06 -0400

    added automated TL to rachel's spreadsheet
    Former-commit-id: 5b6dd5d139c7c5bb9fee53299aa1265112abf633

--- Scripts/Data/Trophic/trawlTrophic.R
+++ Scripts/Data/Trophic/trawlTrophic.R
@@ -63,3 +63,42 @@ setcolorder(foundTLs, c("spp", "common", "trophicLevel", "trophicLevel.se", "phy
 save(foundTLs, file="./trawl/Data/Taxonomy/foundTLs.RData")
 
 write.csv(foundTLs, file="~/Desktop/foundTLS.csv", row.names=FALSE)
+
+
+# ===================================
+# = Combine this w/ rachel's manual =
+# ===================================
+# read rachel's trophic information
+racheltax <- read.csv("/Users/Battrd/Downloads/racheltax.csv")
+racheltax <- data.table(racheltax, key=c("spp","raw.spp","common"))
+
+# just to make the sorting consistent between rachel and computer
+foundTLs <- data.table(foundTLs, key=c("spp","raw.spp","common"))
+
+# check to make sure these data frames match up in rows
+stopifnot(nrow(racheltax)==nrow(foundTLs))
+
+# convert rachel's trophic information to numeric
+racheltax[,c("trophicLevel","trophicLevel.se"):=list(as.numeric(as.character(trophicLevel)), as.numeric(as.character(trophicLevel.se)))]
+
+# indicies of the automated trohpic information
+addTL <- foundTLs[,!is.na(trophicLevel)]
+addTL.se <- foundTLs[,!is.na(trophicLevel.se)]
+
+# indices of rachel's trophic information
+addTL.r <- racheltax[,!is.na(trophicLevel)]
+addTL.se.r <- racheltax[,!is.na(trophicLevel.se)]
+
+# Grab the trophic level information to be added
+TL2add <- foundTLs[addTL&!addTL.r, trophicLevel]
+TLse2add <- foundTLs[addTL.se&!addTL.se.r, trophicLevel.se]
+
+# Add the automated trohpic levels to rachel's spreadsheet
+racheltax[addTL&!addTL.r, trophicLevel:=TL2add]
+racheltax[addTL.se&!addTL.se.r, trophicLevel.se:=TLse2add]
+
+# write the result
+write.csv(racheltax, file="/Users/Battrd/Desktop/rachtax_plus.csv", row.names=FALSE)
+
+
+
