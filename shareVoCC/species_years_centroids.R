5979f46f1c74d1a9f1b9c37cad070405c8bd7b14
rbatt <battrd@gmail.com>
2015-03-23 15:45:35 -0400

    save pinsky centroids in 1 file
    Former-commit-id: 2e1d0fa8e09eda7d28863e3f43a2c01d3b24f79f

---
+++ shareVoCC/species_years_centroids.R
@@ -0,0 +1,63 @@
+
+
+library(bit64)
+library(data.table)
+library(PBSmapping) # for calculating stratum areas
+library(maptools) # for calculating stratum areas
+library(Hmisc)
+library(reshape2)
+
+
+# ===============================
+# = Guess appropriate directory =
+# ===============================
+if(Sys.info()["sysname"]=="Linux"){
+	setwd("~/Documents/School&Work/pinskyPost")
+}else{
+	setwd("~/Documents/School&Work/pinskyPost")
+}
+
+
+# ==================
+# = Load Functions =
+# ==================
+data.location <- "./trawl/Scripts/DataFunctions"
+invisible(sapply(paste(data.location, list.files(data.location), sep="/"), source, .GlobalEnv))
+
+stat.location <- "./trawl/Scripts/StatFunctions"
+invisible(sapply(paste(stat.location, list.files(stat.location), sep="/"), source, .GlobalEnv))
+
+plot.location <- "./trawl/Scripts/PlotFunctions"
+invisible(sapply(paste(plot.location, list.files(plot.location), sep="/"), source, .GlobalEnv))
+
+
+# ======================
+# = Read Centroid Data =
+# ======================
+# Longitude Data
+cent.lat <- fread("./trawl/Science2013/Results/centbiolat_2012-10-16.csv")[,V1:=NULL] # read
+cent.lat.melt <- melt(cent.lat, id.vars=c("regspp", "region", "spp"), variable.name="year", value.name="lat") # format
+setkey(cent.lat.melt, regspp, region, spp, year)
+
+cent.lon <- fread("./trawl/Science2013/Results/centbiolon_2012-10-16.csv")[,V1:=NULL] # read
+cent.lon.melt <- melt(cent.lon, id.vars=c("regspp", "region", "spp"), variable.name="year", value.name="lon") # format
+setkey(cent.lon.melt, regspp, region, spp, year)
+
+
+# ===========================
+# = Merge Centroid Lon/ Lat =
+# ===========================
+centroids <- merge(cent.lat.melt, cent.lon.melt)
+
+
+# centroids[,plot(lon, lat)]
+
+# ==================
+# = Save Centroids =
+# ==================
+write.csv(centroids, file="./trawl/Science2013/Results/pinsky2013_centroids.csv", row.names=FALSE)
+
+
+
+
+
