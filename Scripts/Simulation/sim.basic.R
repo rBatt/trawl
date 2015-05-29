be2be0fa58b47f5313a553d16ed42b1f8df71429
rbatt <battrd@gmail.com>
2015-05-29 15:10:41 -0400

    add colonization and rw to sim; org/ clean; figs
    Former-commit-id: 227b1f03b9bb9bb97ae0371d9d43adc595a849e5

--- Scripts/Simulation/sim.basic.R
+++ Scripts/Simulation/sim.basic.R
@@ -68,12 +68,7 @@ for(i in 2:grid.t){
 	grid.temp <- setValues(grid.temp, values(subset(grid.temp, i-1))+rg()+temp.slope, layer=i)
 }
 
-# Plot temperature time series
 temp.range <- range(values(grid.temp))
-smplt <- c(0.89,0.95, 0.2,0.8)
-bgplt <- c(0.15,0.85,0.12,0.95)
-axargs <- list(mgp=c(0.75,0.25,0), tcl=-0.15)
-plot(grid.temp, zlim=c(temp.range[1], temp.range[2]), col=tim.colors(), smallplot=smplt, bigplot=bgplt, axis.args=axargs, nc=5,mgp=c(0.75,0.25,0), tcl=-0.15)
 
 
 # ==================
@@ -103,53 +98,6 @@ S.obs.temps <- mapply(rtemp, alpha=ab[1,], beta=ab[2,], min=mm[1,], max=mm[2,], 
 S.dens.temps <- apply(S.obs.temps, 2, density, from=temp.range[1], to=temp.range[2], adjust=1) # density (prob) of each species at all possible temperatures
 
 
-#
-# test.t <- seq(-10, 10, length.out=500)
-# dev.new(width=12, height=8)
-# par(mfrow=c(7,10), mar=c(1,1,0,0))
-# for(i in 1:70){
-# 	test.ks <- p.persist(test.t, S.obs.temps[,i], method="ks")
-# 	test.ds <- p.persist(test.t, S.obs.temps[,i], method="ds")
-#
-# 	plot(test.t, test.ks, type="l", ylim=c(0,1))
-# 	lines(test.t, test.ds, col="red")
-# }
-# after seeing these plots, I'm a bit torn about which approach to use. Given that they aren't wildly different, KS test has an advantage b/c it is an established test, and the statistical interpretation of the p-value of this test is spot-on: the probability that a temperature was drawn from the same distribution as the temperature values previously-observed to be associated with the species. On the other hand, the reason the dsample() method gives higher "probabilities" within the previously-observed temperature range is b/c it's assumed to be 0 outside that range; which can be useful in its own right. However, even on the off-chance that the KS p-value allows a species to expand far outside its previous observed temperatures, this isn't all that unreasonable if temperature isn't given too much of a physiological interpretation and is simply viewed as a correlate of suitable habitat. I think that in this case the KS will win out b/c it'll be much easier to explain and defend.
-# Also, I've later realized that dsample() is much faster than doing the mapply with ks.test. I can't think of a way to better program an implementation of ks.test without digging into its source code, because as-is, if x in ks.test is a vector, the probability returned is simply the probability of the x and y distributions being different.
-
-
-# trying to find a way to get skew to go in either direction
-# k0 <- seq(2, 50, length.out=50)
-# lambda0 <- seq(0.1, 5, length.out=50)
-# kl.grid <- expand.grid(k=k0, lambda=lambda0)
-# k <- kl.grid[,"k"]
-# lambda <- kl.grid[,"lambda"]
-# mu <- lambda*gamma(1+1/k)
-# sigma2 <- lambda^2*(gamma(1+2/k)-gamma(1+1/k)^2)
-# skew <- (gamma(1+3/k)*lambda^3 - 3*mu*sigma2 - mu^3)/sqrt(sigma2)^3
-# OK, you can get it with the weibull, but the weibull always has to be positive, so you can only get the left skew when the values are very small and positive
-
-
-# Sdt.max <- max(sapply(S.dens.temps, function(x)max(x$y)))
-# myGray <- rgb(t(col2rgb("black", alpha=TRUE)), alpha=75, maxColorValue=256)
-# dev.new()
-# plot(S.dens.temps[[1]], ylim=c(0,Sdt.max), col=myGray)
-# for(i in 2:length(S.dens.temps)){lines(S.dens.temps[[i]], col=myGray)}
-
-# Plot Temperature Distributions for the Whole Community
-store <- c()
-for(d in 1:512){
-	ti <- c()
-	for(i in 1:length(S.dens.temps)){
-		ti[i] <- S.dens.temps[[i]]$y[d]
-	}
-	store[d] <- mean(ti)
-}
-dev.new(width=3.5, height=3.5)
-par(mar=c(2.15,2.15,0.1,0.1), ps=10, mgp=c(1.15,0.2,0), tcl=-0.2)
-plot(S.dens.temps[[1]]$x, store, type="l", xlab="Temperature", ylab="Density (community average)")
-
-
 # =====================================
 # = Initial Secies Population of Grid =
 # =====================================
@@ -168,58 +116,27 @@ spp.pres[,,1] <- c0[["spp.pres"]]
 spp.bio[,,1] <- c0[["spp.bio"]]
 
 
-# ================================
-# = Graphs of Initial Conditions =
-# ================================
-# Plot Initial Biomass of Each Species
-spp.1 <- setValues(S[[1]], c0[["spp.bio"]]) # cell ordering is for raster
-smplt <- c(0.9,0.92, 0.2,0.8)
-bgplt <- c(0.05,0.89,0.15,0.95)
-axargs <- list(mgp=c(0.75,0.5,0))
-dev.new(width=16, height=8)
-plot(spp.1, maxnl=200, col=tim.colors(), zlim=range(values(spp.1), na.rm=TRUE),smallplot=smplt, bigplot=bgplt, axis.args=axargs, nr=8, nc=25, legend=FALSE, colNA="darkgray")
-
-# Look at Initial Richness
-spp.sample <- function(x, n){
-	sub <- matrix(x[sample(nrow(x), n),], ncol=ncol(x))
-	sum(apply(sub, 2, function(x)any(!is.na(x))))
-}
-
-spp.1.m <- matrix(spp.pres[,,1], nrow=Nv, ncol=ns) # cell ordering is for raster
-rich <- c()
-for(i in 1:min(Nv,200)){
-	rich[i] <- spp.sample(spp.1.m, n=i)
-}
-dev.new()
-plot(rich, type="o", ylim=c(0,ns))
-
-
-R.1 <- stackApply(spp.1, indices=1, function(x, ...)sum(!is.na(x)))
-
-
 # ====================
 # = Spatial Dynamics =
 # ====================
 AR1.coef <- 0.8 # first order autoregressive coefficient for each cell; to be placed on the diagonal of the transition matrix for cells that previous had biomass and presently remain suitable
 D.frac <- 0.5 # fraction of biomass to disperse from local cell to surroundings; think of it like reproduction, such that it doesn't result in a decrease in the biomass of the focal cell
 M.frac <- 1 # proportion of biomass that will leave cell when it becomes unsuitable (this proportion is split evenly among neighbors, but not all neighbors will be suitable, so in many cases less than 100% of the biomass will find a new home)
-
 perist.bonus <- 1 # Factor by which to adjust the probability that a species will fail to persist; decreasing this factors makes the species more likely to stick around, increasing it makes it less likely to stick around. When 1, no adjustment is made; when 0, the species will always stick around.
-
 
 # Create indices to convert between matrix() and raster() conventions for naming/indexing cells
 r2m <- order(c(r2m.cell(1:Nv, nrow=grid.h, ncol=grid.w))) # put this in brackets after a vector of cells ordered by the raster convention to put them in the order of the matrix convention
 m2r <- order(r2m) # use this to reverse the above process
 
-
-for(i in 2:grid.t){
+# Begin looping through time steps to simulate dynamics
+for(i in 2:grid.t){ # through time
 
 	# Suitability of Persistance (p.persist)
 	t.temp <- subset(grid.temp, i)
 	c.t <- colonize(values(t.temp), S.dens.temps, spp.bio.mu)
 	suit.pers[,,i] <- c.t[["suit.pers"]]
 
-	for(s in 2:ns){
+	for(s in 2:ns){ # through species
 	
 		# =========================
 		# = Define Suitable Cells =
@@ -271,23 +188,174 @@ for(i in 2:grid.t){
 		G.t1[G.na] <- 0
 	
 		G <- Trans%*%G.t1
-		G[G==0] <- NA # TODO might want to find a more reliable way of doing this; I could use pers.outcome, but this isn't always reliable because sometimes there are cells 
+		G[G==0] <- NA # TODO might want to find a more reliable way of doing this; I could use pers.outcome, but this isn't always reliable because sometimes there are cells that are suitable but are isolated and did not previously have anything.
+		
+		# ==================================
+		# = Colonize if Previously Extinct =
+		# ==================================
+		if(all(G.na)){ # If completely absent last time step, give it a chance to colonize
+			G[] <- c.t[["spp.bio"]][r2m,s]
+		}
 	
 		# is.na(pers.outcome)[r2m] & G!=0
 		# !is.na(pers.outcome)[r2m] & G==0
 		# all((rowSums(Trans)==0) == (c(G)==0)) # maybe could use the rowSums of the transition matrix instead of the G==0
 		
+		
+		# ===============
+		# = Random Walk =
+		# ===============
+		G <- G + rnorm(n=length(G), mean=0, sd=0.1)
+		
+		
+		# ====================
+		# = Fill in Matrices =
+		# ====================
 		spp.pres[r2m,s,i][!is.na(G)] <- 1
 		spp.bio[r2m,s,i] <- G
-	
-
-	
 	
 	}
 }
 
 
+# ==========
+# = Graphs =
+# ==========
 
+
+# ===========================
+# = Temperature Time Series =
+# ===========================
+# Plot temperature time series
+temp.range <- range(values(grid.temp))
+smplt <- c(0.89,0.95, 0.2,0.8)
+bgplt <- c(0.15,0.85,0.12,0.95)
+axargs <- list(mgp=c(0.75,0.25,0), tcl=-0.15)
+dev.new()
+plot(grid.temp, zlim=c(temp.range[1], temp.range[2]), col=tim.colors(), smallplot=smplt, bigplot=bgplt, axis.args=axargs, nc=5,mgp=c(0.75,0.25,0), tcl=-0.15)
+
+
+# =======================================
+# = Species/ Community Richness Density =
+# =======================================
+# Plot Temperature Distributions for the Whole Community
+store <- c()
+sdt2 <- lapply(S.dens.temps, FUN=function(x){x$y <- x$y/max(x$y); x})
+for(d in 1:512){
+	ti <- c()
+	for(i in 1:length(sdt2)){
+		ti[i] <- sdt2[[i]]$y[d]
+	}
+	store[d] <- mean(ti)
+}
+dev.new(width=3.5, height=3.5)
+par(mar=c(2.15,2.15,0.1,0.1), ps=10, mgp=c(1.15,0.2,0), tcl=-0.2)
+plot(S.dens.temps[[1]]$x, store, type="l", xlab="Temperature", ylab="Density (community average)")
+
+# dev.new()
+# plot(sdt2[[1]]$x, sdt2[[1]]$y, type="l", ylim=c(0,1))
+# for(i in 2:ns){
+# 	lines(sdt2[[i]]$x, sdt2[[i]]$y, type="l")
+# }
+
+# weighted.mean(S.dens.temps[[1]]$x, w=store)
+# sum(store[S.dens.temps[[1]]$x>0])
+# sum(store[S.dens.temps[[1]]$x<0])
+
+# ===============================================
+# = Initial Biomass and Presence of All Species =
+# ===============================================
+# Plot Initial Biomass of Each Species
+spp.1 <- setValues(S[[1]], c0[["spp.bio"]]) # cell ordering is for raster
+smplt <- c(0.9,0.92, 0.2,0.8)
+bgplt <- c(0.05,0.89,0.15,0.95)
+axargs <- list(mgp=c(0.5,0.15,0))
+dev.new(width=12, height=5)
+plot(spp.1, maxnl=200, col=tim.colors(), zlim=range(values(spp.1), na.rm=TRUE),smallplot=smplt, bigplot=bgplt, axis.args=axargs, nr=8, nc=25, legend=FALSE, colNA="darkgray")
+
+
+# =============================================
+# = Final Biomass and Presence of All Species =
+# =============================================
+# Plot Initial Biomass of Each Species
+spp.final <- setValues(S[[1]], spp.bio[,,grid.t]) # cell ordering is for raster
+smplt <- c(0.9,0.92, 0.2,0.8)
+bgplt <- c(0.05,0.89,0.15,0.95)
+axargs <- list(mgp=c(0.5,0.15,0))
+dev.new(width=12, height=5)
+plot(spp.final, maxnl=200, col=tim.colors(), zlim=range(values(spp.final), na.rm=TRUE),smallplot=smplt, bigplot=bgplt, axis.args=axargs, nr=8, nc=25, legend=FALSE, colNA="darkgray")
+
+
+# =====================================
+# = True Richness over Time and Space =
+# =====================================
+space.rich <- grid.blank
+for(i in 1:grid.t){
+	t.bio <- spp.bio[,,i]
+	t.rich <- apply(t.bio, 1, function(x, ...)sum(!is.na(x)))
+	values(space.rich[[i]]) <- t.rich
+	
+}
+dev.new()
+plot(space.rich, col=tim.colors(), zlim=range(values(space.rich)))
+
+
+# ===========================
+# = True Richness Over Time =
+# ===========================
+true.rich <- c()
+for(i in 1:grid.t){
+	t.bio <- spp.bio[,,i]
+	true.rich[i] <- sum(apply(t.bio, 2, function(x, ...)any(!is.na(x))))
+}
+dev.new(width=3.5, height=3.5)
+par(mar=c(2, 2, 0.1, 0.1), mgp=c(1.1, 0.15, 0), tcl=-0.15, ps=10)
+plot(1:grid.t, true.rich, type="l", xlab="Time", ylab="Total Grid Richness")
+
+
+# ===================================
+# = Richness Accumulation over Time =
+# ===================================
+dev.new(width=3.5, height=3.5)
+par(mar=c(1.75, 1.75, 0.1, 0.1), mgp=c(0.85, 0.15, 0), tcl=-0.15, ps=10)
+
+spp.sample <- function(x, n){
+	sub <- matrix(x[sample(nrow(x), n),], ncol=ncol(x))
+	sum(apply(sub, 2, function(x)any(!is.na(x))))
+}
+
+
+rich.cols <- tim.colors(n=grid.t)
+
+n.max <- min(Nv,200)
+n.iter <- 5
+min.effort <- 1
+
+for(j in 1:grid.t){
+	spp.t.m <- matrix(spp.pres[,,j], nrow=Nv, ncol=ns) # cell ordering is for raster
+
+	rich <- matrix(NA, nrow=n.max, ncol=n.iter)
+	for(k in 1:n.iter){
+		for(i in min.effort:n.max){
+			rich[i,k] <- spp.sample(spp.t.m, n=i)
+		}
+	}
+	
+	rich.mu <- rowMeans(rich)
+	
+	if(j==1){
+		plot(1:n.max, rich.mu, col=rich.cols[j], ylab="Avg Richness", xlab="# Cells Sampled", type="l", ylim=c(50,max(true.rich)))
+	}else{
+		lines(1:n.max, rich.mu, col=rich.cols[j])
+	}
+	
+	
+}
+
+
+# =============================================
+# = Results of Simulation over Time and Space =
+# =============================================
 # Plot up transition results
 smplt <- c(0.85,0.88, 0.2,0.8)
 bgplt <- c(0.05,0.82,0.15,0.95)
@@ -296,7 +364,7 @@ axargs <- list(mgp=c(0.5,0.15,0))
 dev.new(width=5, height=2)
 
 n.steps <- 6
-spp.id <- 9
+spp.id <- 3
 
 par(mfrow=c(2, n.steps), oma=c(0.5,1,0,0))
 year.index <- seq(1, grid.t, length.out=n.steps)
@@ -310,11 +378,3 @@ for(i in 1:n.steps){
 	image.plot(t(matrix(spp.bio[,spp.id,t.y], nrow=grid.h, byrow=TRUE)), zlim=range(spp.bio[,spp.id,], na.rm=TRUE), ylim=c(1.085,0), bg="gray", axis.args=axargs, xaxt="n", yaxt="n", smallplot=smplt, bigplot=bgplt)
 }
 
-
-
-
-# ============
-# = Colonize =
-# ============
-# Give "extinct" species a chance to colonize.
-colSums(spp.pres[,,1], na.rm=TRUE)==0
