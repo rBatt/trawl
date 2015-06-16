5b357591fefc458a603cd80f60f8f720d56c7a02
rbatt <battrd@gmail.com>
2015-06-16 17:08:49 -0400

    fix colors and add plot of shape
    Former-commit-id: eb720ed7aa6006ad6a3cd1377f5a2b2e04614876

--- Scripts/Data/Trophic/rachelPractice.R
+++ Scripts/Data/Trophic/rachelPractice.R
@@ -1,7 +1,5 @@
 
-library(plyr)
-library(reshape)
-library(reshape2)
+library(RColorBrewer)
 library(data.table)
 
 # ===============================
@@ -38,22 +36,64 @@ yrs <- unique(tt.ai[,"year"]) # unique years
 # dev.new(width=3.5, height=3.5) # doesn't work in R studio
 par(mar=c(2.25,2.25,0.1,0.1), ps=10, mgp=c(1.15, 0.25, 0), tcl=-0.15)
 ylims <- range(sqrt(tt.ai[,"wtcpue.sum"]), na.rm=TRUE)
+cols <- brewer.pal(n=length(tgs), name="Paired")
 
 tg1.index <- tt.ai[,"trophicGroup"] == tgs[1]
-plot(x=tt.ai[tg1.index,"year"], y=sqrt(tt.ai[tg1.index,"wtcpue.sum"]), type="o", ylab="mass", xlab="year", ylim=ylims)
+plot(x=tt.ai[tg1.index,"year"], y=sqrt(tt.ai[tg1.index,"wtcpue.sum"]), type="o", ylab="mass", xlab="year", ylim=ylims, col=cols[1], pch=20)
 
 
+# loop through trophic groups, plotting a line for each one
 for(i in 2:length(tgs)){
-	print(i)
 	t.tgi <- tt.ai[,"trophicGroup"] == tgs[i]
-	lines(x=tt.ai[t.tgi,"year"], y=sqrt(tt.ai[t.tgi,"wtcpue.sum"]), type="o", col=i)	
+	lines(x=tt.ai[t.tgi,"year"], y=sqrt(tt.ai[t.tgi,"wtcpue.sum"]), type="o", col=cols[i], pch=20)	
 }
-legend("topleft",legend=tgs, text.col=1:length(tgs), ncol=2)
+legend("topleft",legend=tgs, text.col=cols, ncol=2, cex=0.85) # add a legend, the inset argument adjusts the relative position of the legend, given the "topleft" starting location.
 
-for(dog in letters){
-	print(dog)
+# legend("topleft",legend=tgs, text.col=cols, ncol=2, inset=c(-0.075,-0.025), cex=0.85) # add a legend, the inset argument adjusts the relative position of the legend, given the "topleft" starting location. The appearence will change depending on the size of the graphical device, which I set via dev.new, and which doesn't work in RStudio. So I'm commenting this line out.
+
+
+# Plot shape over time
+tt.ai.shape <- tt.ai[,c("year","trophicGroup","wtcpue.sum")]
+tt.ai.shape[,"wtcpue.rel"] <- NA
+
+# go through each trophicGroup, and turn it's wtcpue value into a value relative to its biggest value
+for(i in 1:length(tgs)){
+	t.i <- tt.ai.shape[,"trophicGroup"] == tgs[i]
+	t.wtcpue <- tt.ai.shape[t.i,"wtcpue.sum"]
+	tt.ai.shape[t.i,"wtcpue.rel"] <- tt.ai.shape[t.i,"wtcpue.sum"]/max(t.wtcpue, na.rm=TRUE)
 }
 
-# second trophic group
+
+# function to automatically determine the panel dimensions given a desired number of panels in a plot
+auto.mfrow <- function(x, tall=FALSE){
+	stopifnot(x>0)
+	
+	if(x==1L){return(c(1,1))}
+	
+	ran <- 2:max(floor((x-1)/2),1)
+	ran2 <- pmax(ceiling(x/(ran)),1)
+	rem <- abs(x - ran2*ran)
+	
+	score <- abs(sqrt(x)-(ran)) + abs(sqrt(x)-(ran2)) + rem
+	mf1 <- ran[which.min(score)]
+	mf2 <- ran2[which.min(score)]
+	
+	if(!tall){
+		return(sort(c(mf1, mf2)))
+	}else{
+		return(sort(c(mf1, mf2), TRUE))
+	}	
+}
+
+
+
+par(mfrow=auto.mfrow(length(yrs)))
+for(i in 1:length(yrs)){
+	yr.i <- tt.ai.shape[,"year"] == yrs[i]
+	t.dat <- tt.ai.shape[yr.i,]
+	
+	barplot(t.dat[,"wtcpue.rel"], horiz=TRUE, xlim=c(-1,1))
+	barplot(-t.dat[,"wtcpue.rel"], horiz=TRUE, xlim=c(-1,1), add=TRUE, names.arg=tgs, las=1)
+}
 
 
