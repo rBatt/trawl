0814f8eae6909d6147f16d473f969968d4e272e2
rbatt <battrd@gmail.com>
2015-07-30 11:20:19 -0400

    adds reps, add options, clean up

--- Scripts/Simulation/sim.basic.R
+++ Scripts/Simulation/sim.basic.R
@@ -2,11 +2,40 @@
 # =================
 # = Load Packages =
 # =================
+# Data structure
 library(raster)
+library(data.table)
+
+# Graphing
 library(fields)
+
+# Statistics
 library(igraph)
 library(R2jags)
-library(data.table)
+
+# Computing
+library(parallel)
+library(doParallel)
+library(foreach)
+
+# Other
+library(rbLib) # library(devtools); install_github("rBatt/rbLib")
+
+
+# =====================================
+# = Set cores for parallel processing =
+# =====================================
+if(Sys.info()["sysname"]=="Windows"){
+	nC <- floor(detectCores()*0.75)
+	registerDoParallel(cores=nC)
+}else if(Sys.info()["sysname"]=="Linux"){
+	# registerDoParallel(cores=min(c(25,floor(detectCores()*0.75))))
+	registerDoParallel(floor(detectCores()*0.50))
+	# registerDoParallel(floor(detectCores()*0.90))
+}else{
+	registerDoParallel()
+}
+
 
 
 # ===============================
@@ -40,11 +69,30 @@ grid.w <- 5 # Width
 grid.h <- 7 # Height
 grid.t <- 12 # Time
 
+
 # ===================
 # = Species Options =
 # ===================
-# Number of Species
-ns <- 100
+ns <- 100 # Number of Species
+
+
+# ======================
+# = Simulation Options =
+# ======================
+n.obs.reps <- 5 # number of time to observe the same true process (each observation is analyzed separately)
+n.ss <- 9 # number of substrata (for observation)
+n.ss.mu <- max(trunc((n.ss*grid.w*grid.h)/3), grid.w*grid.h) # total substrata observed
+n.noID <- ns/2 # number of species to not be ID'd in first half of time series
+base.chance <- runif(n=ns, 0.2, 0.8) # baseline detectability (before ID chance)
+
+
+# ================
+# = MSOM Options =
+# ================
+nChains <- 3
+nIter <- 4E3
+n0s <- 100
+nThin <- 60 # max(1, floor((nIter - floor(nIter/2)) / 1000))
 
 
 # =================================
@@ -66,52 +114,41 @@ dims <- attr(out, "dims")
 S <- getS(out)
 
 
-
-# ================================
-# = Simulate Observation of True =
-# ================================
-out.obs <- obs.spp(out)
-
-
-# ==========================================
-# = Format Simulation for Analysis by MSOM =
-# ==========================================
-formatted <- spp2msom(out.obs)
-simDat <- formatted$simDat 
-simCov <- formatted$simCov 
-simCov.NA <- formatted$simCov.NA 
-simCov.precs <- formatted$simCov.precs 
-simCov.precs.bad <- formatted$simCov.precs.bad 
-sim.cov.names <- formatted$sim.cov.names
-
-
-# =====================================
-# = Set cores for parallel processing =
-# =====================================
-if(Sys.info()["sysname"]=="Windows"){
-	nC <- floor(detectCores()*0.75)
-	registerDoParallel(cores=nC)
-}else if(Sys.info()["sysname"]=="Linux"){
-	# registerDoParallel(cores=min(c(25,floor(detectCores()*0.75))))
-	registerDoParallel(floor(detectCores()*0.60))
-	# registerDoParallel(floor(detectCores()*0.90))
-}else{
-	registerDoParallel()
+# =========================================
+# = Observation Level and Format for MSOM =
+# =========================================
+# The loop is for re-observing the same true process multiple times
+for(i in 1:n.obs.reps){
+	if(i==1){
+		out.obs <- obs.spp(out, n.ss=9, n.ss.mu, n.noID, base.chance)
+		formatted <- spp2msom(out.obs)
+		new.simDat <- formatted$simDat 
+		simCov <- formatted$simCov 
+		simCov.NA <- formatted$simCov.NA 
+		simCov.precs <- formatted$simCov.precs 
+		simCov.precs.bad <- formatted$simCov.precs.bad 
+		sim.cov.names <- formatted$sim.cov.names
+		
+		big.out.obs <- list(out.obs)
+		
+		names(new.simDat) <- paste(names(new.simDat),i, sep=".")
+		big.simDat <- new.simDat
+	}else{
+		big.out.obs[[i]] <- obs.spp(out, n.ss=9, n.ss.mu, n.noID, base.chance)
+		new.simDat <- spp2msom(big.out.obs[[i]])$simDat
+		names(new.simDat) <- paste(names(new.simDat),i, sep=".")
+		big.simDat <- c(big.simDat, new.simDat)
+	}
 }
 
 
 # =============================
 # = Analyze simData with MSOM =
 # =============================
-nChains <- 3
-nIter <- 4E3
-n0s <- 100
-nThin <- 60 # max(1, floor((nIter - floor(nIter/2)) / 1000))
-
 # Run all other Bayesian richness in parallel
-sim.rich.cov <- foreach(i=(1:length(simDat))) %dopar%{ # run all other subsets in parallel
+sim.rich.cov <- foreach(i=(1:length(big.simDat))) %dopar%{ # run all other subsets in parallel
 	rich.cov(
-		data=simDat[[i]], 
+		data=big.simDat[[i]], 
 		covs=list(simCov.NA[[i]],simCov[[i]]), 
 		cov.precs=list(simCov.precs.bad[[i]], simCov.precs[[i]]), 
 		nameID=paste(sim.cov.names[i,], collapse="_"),
@@ -123,6 +160,7 @@ sim.rich.cov <- foreach(i=(1:length(simDat))) %dopar%{ # run all other subsets i
 		save.fit.cov.dir="trawl/Results/Simulation/"
 	) # do analysis for this subset
 }
+
 
 # ===============
 # = Save Output =
