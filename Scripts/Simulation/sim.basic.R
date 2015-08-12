fa2e57bd9ecb5e0fc773dc1ece00da485558bf28
rbatt <battrd@gmail.com>
2015-08-12 18:35:40 -0400

    needs to be commented before mirror to amphiprion

--- Scripts/Simulation/sim.basic.R
+++ Scripts/Simulation/sim.basic.R
@@ -260,27 +260,27 @@ file.copy(from=saveFile_c, to=renameNow(saveFile_c), copy.date=TRUE)
 # =================================
 # = Run this script on amphiprion =
 # =================================
-# Make sure the
-library(rbLib)
-oldwd <- getwd()
-setwd("~")
-
-# Mirror
-from <- "Documents/School&Work/pinskyPost/trawl/"
-to <- "ryanb@amphiprion.deenr.rutgers.edu:'Documents/School&Work/pinskyPost/trawl/'"
-mirror(from, to)
-
-# Push, Run, Pull
-path <- "./Documents/School&Work/pinskyPost/trawl/Scripts/Simulation/"
-scriptName <- "sim.basic.R"
-remoteName <- "ryanb@amphiprion.deenr.rutgers.edu"
-push(path, remoteName, fileName=scriptName)
-run(scriptName, path, remoteName)
-pull(path, remoteName)
-# prp(path, scriptName, remoteName, verbose=TRUE)
-
-# Pull whole trawl
-pull("./Documents/School&Work/pinskyPost/trawl/", remoteName)
-
-setwd(oldwd)
+# # Make sure the
+# library(rbLib)
+# oldwd <- getwd()
+# setwd("~")
+#
+# # Mirror
+# from <- "Documents/School&Work/pinskyPost/trawl/"
+# to <- "ryanb@amphiprion.deenr.rutgers.edu:'Documents/School&Work/pinskyPost/trawl/'"
+# mirror(from, to)
+#
+# # Push, Run, Pull
+# path <- "./Documents/School&Work/pinskyPost/trawl/Scripts/Simulation/"
+# scriptName <- "sim.basic.R"
+# remoteName <- "ryanb@amphiprion.deenr.rutgers.edu"
+# push(path, remoteName, fileName=scriptName)
+# run(scriptName, path, remoteName)
+# pull(path, remoteName)
+# # prp(path, scriptName, remoteName, verbose=TRUE)
+#
+# # Pull whole trawl
+# pull("./Documents/School&Work/pinskyPost/trawl/", remoteName)
+#
+# setwd(oldwd)
 
