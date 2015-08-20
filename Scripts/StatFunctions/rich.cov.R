4da62420e2e21968efcb84ef18b0f3b61d23941b
rbatt <battrd@gmail.com>
2015-08-20 16:43:59 -0400

    comment out all the plotting diagnostics
    fml

--- Scripts/StatFunctions/rich.cov.R
+++ Scripts/StatFunctions/rich.cov.R
@@ -188,46 +188,46 @@ rich.cov <- function(data, covs, cov.precs, nameID, nzeroes=100, nChains=3, nIte
 	# out <- list(mean=fit.cov$BUGSoutput$mean, median=fit.cov$BUGSoutput$median, sd=fit.cov$BUGSoutput$sd)
 	# out <- list(mean=fit.cov$BUGSoutput$mean, BUGSoutput=fit.cov$BUGSoutput)
 
-	library(ggmcmc)
-	samples.binary <- coda.samples(fit.cov$model, c("w","Z"), 100)
-	samples.cont <- coda.samples(fit.cov$model, c("u.a0","a3","a4", "psi","v.a0","p"), 100)
-	samples.binary.Z <- ggs(samples.binary, family="Z")
-	samples.cont.2 <- ggs(samples.cont)
-	Z.outcome <- c(t(apply(Xaug1, c(1,3), max, na.rm=TRUE)))
-	ggmcmc(samples.cont.2, "~/Desktop/test.pdf", width=12, height=12, param_page=5)
-	Z.roc <- ggs_rocplot(samples.binary.Z, outcome=Z.outcome)
-
-	sims <- fit.cov$BUGSoutput$sims.matrix
-
-	# add mu.psi to sims
-	w.ind <- grepl("w\\[.*", colnames(sims))
-	psi.ind <- grepl("psi.*", colnames(sims))
-	mu.psi <- rep(sims[,w.ind],each=grid.w*grid.h)*sims[,psi.ind]
-	colnames(mu.psi) <- paste0("mu.",colnames(mu.psi))
-
-	# add mu.p to sims
-	# TODO Need to finish this – computer crashed when i did something stupid; need to check indices on p and Z to do this calculation correctly; i know the repeating isn't right
-	Z.ind <- grepl("Z\\[.*", colnames(sims))
-	p.ind <- grepl("p\\[.*", colnames(sims))
-	mu.p <- rep(sims[,Z.ind],each=grid.w*grid.h)*sims[,p.ind]
-	colnames(mu.p) <- paste0("mu.",colnames(mu.p))
-
-	out.mode <- apply(sims, 2, mode)
-	out.med <- apply(sims, 2, median)
-	out.mean <- apply(sims, 2, mean)
-
-
-	centrals <- data.frame(mode=out.mode, med=out.med, mean=out.mean)
-
-
-	mu.psi <-
-	centrals.psi <- cbind(centrals[psi.ind,][1:(ns*grid.w*grid.h),], true.mean=c(psi.true[,,1,1]))
-	centrals.w <- centrals[w.ind,]
-	pairs(centrals.psi, panel=function(x,y,...){points(x,y, ...);abline(a=0,b=1)}, pch=20, cex=0.5, col=adjustcolor("black",alpha.f=0.2))
-
-	table(gsub("\\[.*\\]", "", colnames(sims))) # number of parameters per main parameter
-	
-	out <- list(mean=fit.cov$BUGSoutput$mean, median=fit.cov$BUGSoutput$median)
+	# library(ggmcmc)
+# 	samples.binary <- coda.samples(fit.cov$model, c("w","Z"), 100)
+# 	samples.cont <- coda.samples(fit.cov$model, c("u.a0","a3","a4", "psi","v.a0","p"), 100)
+# 	samples.binary.Z <- ggs(samples.binary, family="Z")
+# 	samples.cont.2 <- ggs(samples.cont)
+# 	Z.outcome <- c(t(apply(Xaug1, c(1,3), max, na.rm=TRUE)))
+# 	ggmcmc(samples.cont.2, "~/Desktop/test.pdf", width=12, height=12, param_page=5)
+# 	Z.roc <- ggs_rocplot(samples.binary.Z, outcome=Z.outcome)
+#
+# 	sims <- fit.cov$BUGSoutput$sims.matrix
+#
+# 	# add mu.psi to sims
+# 	w.ind <- grepl("w\\[.*", colnames(sims))
+# 	psi.ind <- grepl("psi.*", colnames(sims))
+# 	mu.psi <- rep(sims[,w.ind],each=grid.w*grid.h)*sims[,psi.ind]
+# 	colnames(mu.psi) <- paste0("mu.",colnames(mu.psi))
+#
+# 	# add mu.p to sims
+# 	# TODO Need to finish this – computer crashed when i did something stupid; need to check indices on p and Z to do this calculation correctly; i know the repeating isn't right
+# 	Z.ind <- grepl("Z\\[.*", colnames(sims))
+# 	p.ind <- grepl("p\\[.*", colnames(sims))
+# 	mu.p <- rep(sims[,Z.ind],each=grid.w*grid.h)*sims[,p.ind]
+# 	colnames(mu.p) <- paste0("mu.",colnames(mu.p))
+#
+# 	out.mode <- apply(sims, 2, mode)
+# 	out.med <- apply(sims, 2, median)
+# 	out.mean <- apply(sims, 2, mean)
+#
+#
+# 	centrals <- data.frame(mode=out.mode, med=out.med, mean=out.mean)
+#
+#
+# 	mu.psi <-
+# 	centrals.psi <- cbind(centrals[psi.ind,][1:(ns*grid.w*grid.h),], true.mean=c(psi.true[,,1,1]))
+# 	centrals.w <- centrals[w.ind,]
+# 	pairs(centrals.psi, panel=function(x,y,...){points(x,y, ...);abline(a=0,b=1)}, pch=20, cex=0.5, col=adjustcolor("black",alpha.f=0.2))
+#
+# 	table(gsub("\\[.*\\]", "", colnames(sims))) # number of parameters per main parameter
+#
+# 	out <- list(mean=fit.cov$BUGSoutput$mean, median=fit.cov$BUGSoutput$median)
 	
 	# out <- list(blah=rnorm(5))
 	#
