22080452e58dc46106eccb104460a7b47cb4fb5d
rbatt <battrd@gmail.com>
2015-08-21 06:43:56 -0400

    reduce simulation extent
    need fast results
    
    
    Former-commit-id: d1a41656e4e59095f88d97265982a004b9fb85ff

--- Scripts/Simulation/sim.basic.R
+++ Scripts/Simulation/sim.basic.R
@@ -66,7 +66,7 @@ if(Sys.info()["sysname"]=="Windows"){
 # Grid Size
 grid.w <- 6 # Width # 6
 grid.h <- 9 # Height # 11
-grid.t <- 8 # Time
+grid.t <- 4 # Time
 
 
 # ===================
@@ -78,7 +78,7 @@ ns <- 50 # Number of Species
 # ======================
 # = Simulation Options =
 # ======================
-n.obs.reps <- 8 # number of time to observe the same true process (each observation is analyzed separately)
+n.obs.reps <- 4 # number of time to observe the same true process (each observation is analyzed separately)
 n.ss <- 4 # number of substrata (for observation)
 n.ss.mu <- n.ss*grid.w*grid.h #max(trunc((n.ss*grid.w*grid.h)/3*2), grid.w*grid.h) # total substrata observed
 base.chance <- 1 #plogis(rnorm(ns)) #rbeta(ns,2,2) #runif(n=ns, 0.2, 0.8) # baseline detectability (before ID chance)
@@ -157,7 +157,7 @@ t.noID <- plogis(obs.chance(dim2=ns, dim1=grid.t, dim3=n.obs.reps, mean=t.noID.m
 # ================
 # = MSOM Options =
 # ================
-nChains <- 4
+nChains <- 3
 nIter <- 5E4
 n0s <- 50
 nSamples <- 500
