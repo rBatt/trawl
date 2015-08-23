b0088445956bf169e6235ebd20cd7eb19c033012
rbatt <battrd@gmail.com>
2015-08-23 16:48:22 -0400

    increase substrata
    this is something I haven't really tried much yet, and might help. I've been noticing that the msom doesn't do much better than observations for site-specific richness, so long as the chance of observing species is decent. But this happens even when the MSOM does OK at estimating psi. It just seems overly fragile to detectability. Currently, the best idea I have for overcoming this fragility is to increase the number of substrata - this should give the model a better handle of detectability via sample size.
    
    
    Former-commit-id: 8cd623e964dd24927389fbb81a0f5c99014d4a37

--- Scripts/Simulation/sim.basic.R
+++ Scripts/Simulation/sim.basic.R
@@ -64,8 +64,8 @@ if(Sys.info()["sysname"]=="Windows"){
 # = Grid Options =
 # ================
 # Grid Size
-grid.w <- 20 # Width # 6
-grid.h <- 20 # Height # 11
+grid.w <- 12 # Width # 6
+grid.h <- 12 # Height # 11
 grid.t <- 4 # Time
 
 
@@ -79,8 +79,8 @@ ns <- 40 # Number of Species
 # = Simulation Options =
 # ======================
 n.obs.reps <- 8 # number of time to observe the same true process (each observation is analyzed separately)
-n.ss <- 4 # number of substrata (for observation)
-n.ss.mu <- (n.ss*grid.w*grid.h)*(75/100) #max(trunc((n.ss*grid.w*grid.h)/3*2), grid.w*grid.h) # total substrata observed
+n.ss <- 25 # number of substrata (for observation)
+n.ss.mu <- trunc((n.ss*grid.w*grid.h)*(50/100)) #max(trunc((n.ss*grid.w*grid.h)/3*2), grid.w*grid.h) # total substrata observed
 base.chance <- 1 #plogis(rnorm(ns)) #rbeta(ns,2,2) #runif(n=ns, 0.2, 0.8) # baseline detectability (before ID chance)
 
 # Create chance to be identified if caught
@@ -286,7 +286,7 @@ file.copy(from=saveFile_b, to=renameNow(saveFile_b), copy.date=TRUE)
 # =================================
 # = Run this script on amphiprion =
 # =================================
-# Make sure the
+# # Make sure the
 # library(rbLib)
 # oldwd <- getwd()
 # setwd("~")
