509edab9feb5a435c6e7150cb9ddb3fc4b4cbfaa
rbatt <battrd@gmail.com>
2015-08-23 00:06:43 -0400

    more reps
    Former-commit-id: ff8a900e8279db0128f29248fbca917c7e3e1ea9

--- Scripts/Simulation/sim.basic.R
+++ Scripts/Simulation/sim.basic.R
@@ -78,7 +78,7 @@ ns <- 40 # Number of Species
 # ======================
 # = Simulation Options =
 # ======================
-n.obs.reps <- 4 # number of time to observe the same true process (each observation is analyzed separately)
+n.obs.reps <- 8 # number of time to observe the same true process (each observation is analyzed separately)
 n.ss <- 4 # number of substrata (for observation)
 n.ss.mu <- (n.ss*grid.w*grid.h)*(75/100) #max(trunc((n.ss*grid.w*grid.h)/3*2), grid.w*grid.h) # total substrata observed
 base.chance <- 1 #plogis(rnorm(ns)) #rbeta(ns,2,2) #runif(n=ns, 0.2, 0.8) # baseline detectability (before ID chance)
