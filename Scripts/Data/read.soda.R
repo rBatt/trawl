23425a403194a5ed6946ba19e693d5dc13fe6e82
rbatt <battrd@gmail.com>
2015-09-17 18:50:20 -0400

    sketch how to accumulate bottom temperatures
    Former-commit-id: e1bb6cf425fc2d0506f2c605a7c7f0c8204959ca

--- Scripts/Data/read.soda.R
+++ Scripts/Data/read.soda.R
@@ -21,8 +21,21 @@ soda.time <- seq.Date(time.start, by="1 month", length.out=ntime)
 
 for(i in 1:ntime){
 	t.soda1 <- brick("./Data/SODA/soda_temp_180W-20W_10N-85N_1958-1978.nc", lvar=4, level=i)
+	# need to switch missing value to actual NA
 	soda.depths <- as.numeric(gsub("X", "", names(soda1))) # does weird rounding that I don't understand
-	for(i in )
+	for(j in ndepth:1){
+		if(j==ndepth){
+			# get the deepest temperature to fill in the object that will eventually have all bot temp
+		}else{
+			# are there cells where the accumulated object has NA, but current depth has temp?
+			# if so, replace NA's in accumulated grid with the temperatures of this current depth
+		}
+		# 
+		
+	}
+	# the j loop gives the bottom temperature for a given month
+	# need to go on accumulating months
+	# this will be for the first time period (1958-1978)
 }
 
 
