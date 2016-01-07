787eba0a87b2af35a2962494fe58982a5301a808
rBatt <battrd@gmail.com>
2016-01-07 17:42:28 -0500

    initial commit for trawTL package
    Former-commit-id: c7fd14990a45a778849af3f4e31a946ce198ac06

---
+++ trawlTL/R/trophic_shape.R
@@ -0,0 +1,8 @@
+#' Calculate the trophic shape of a food web
+#' 
+#' Yap
+#' 
+#' @param tl numeric vector of trophic positions
+#' @param w numeric vector of weights; if missing, all weighted equally
+#' 
+#' 
