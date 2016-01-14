58b9ee805c60754e1e2bdaa7d8458fffd2abe1e2
rBatt <battrd@gmail.com>
2016-01-13 20:59:36 -0500

    add back in old comments, more plotting, add getShape

--- trawlTL/R/trophic_shape.R
+++ trawlTL/R/trophic_shape.R
@@ -6,7 +6,7 @@
 #' @param t_res trophic level resolution used in binning
 #' 
 #' @examples
-neus_ts <- trophic_shape("shelf", t_res=0.5)
+neus_ts <- trophic_shape("neus", t_res=0.25)
 
 mass_l <- neus_ts$mass_l
 rich_l <- neus_ts$rich_l
@@ -18,10 +18,33 @@ rich_l <- neus_ts$rich_l
 
 library(fields)
 image.plot(x=mass_l$x, y=mass_l$y, z=mass_l$z, xlab="year", ylab="trophic level")
-lines(x=mass_l$x, y=mass_l$bulge, col="white", lwd=2)
+lines(x=mass_l$x, y=mass_l$mid, col="white", lwd=2)
+
+sh <- getShape(skew=mass_l$sk, bulge=mass_l$bul)
+t_points(mass_l$x, mass_l$mid, shape=sh, inches=0.2, col="black", lwd=3)
+t_points(mass_l$x, mass_l$mid, shape=sh, inches=0.2, col="white", lwd=0.5)
 
 image.plot(x=rich_l$x, y=rich_l$y, z=rich_l$z, xlab="year", ylab="trophic level")
-lines(x=rich_l$x, y=rich_l$bulge, col="white", lwd=2)
+lines(x=rich_l$x, y=rich_l$mid, col="white", lwd=2)
+
+
+
+Xa[spp=="Gadus morhua",plot(year, cntcpue, type="l")]
+Xa[spp=="Gadus morhua",j={par(new=T);plot(year, wtcpue, type="l", xaxt="n", yaxt='n', xlab="", ylab="", col="red");axis(side=4)}]
+
+
+Xa[spp=="Squalus acanthias",plot(year, cntcpue, type="l")]
+Xa[spp=="Squalus acanthias",j={par(new=T);plot(year, wtcpue, type="l", xaxt="n", yaxt='n', xlab="", ylab="", col="red");axis(side=4)}]
+
+Xa[,]
+
+par(rbLib::automfrow(Xa[,lu(tg)]))
+
+Xa2[,]
+
+
+
+
 
 trophic_shape <- function(reg, t_res=0.5){
 
@@ -48,10 +71,22 @@ trophic_shape <- function(reg, t_res=0.5){
 	Xa[,tg:=as.character(cut(trophicLevel, breaks=cut_seq, labels=cut_seq[-1], include.lowest=TRUE))]
 	Xa[is.na(tg) & trophicLevel>=max(cut_seq), tg:=as.character(max(cut_seq))]
 	Xa[,m:=wtcpue]
+	# Xa[,m:=cntcpue]
 	Xa[,nObs:=nAgg]
 	Xa[,nAgg:=NULL]
 	Xa[,r:=1]
 	Xa[,datetime:=as.POSIXct(year, format="%Y")]
+	
+	
+	# Xs <- Xa[tg=="4",reshape2::acast(.SD, year~spp, value.var="cntcpue", fill=0)]
+	# ss <- apply(Xs, 2, var)
+	# lcbd <- ss/sum(ss)
+	# sort(lcbd)
+	#
+	# plot(as.integer(rownames(Xs)), Xs[,1], type='l', ylim=range(Xs))
+	# for(i in 2:ncol(Xs)){
+	# 	lines(as.integer(rownames(Xs)), Xs[,i], col=i+1)
+	# }
 	
 	
 	Xa2 <- trawlAgg(Xa, bio_lvl="tg", time_lvl="year", space_lvl="reg", bioCols=c("nObs","r","m"), envCols=c("stemp","btemp","depth"), bioFun=sum, envFun=mean, metaCols="trophicLevel.se", meta.action="FUN", metaFun=function(x, ...)sum(x^2, ...))
@@ -67,26 +102,59 @@ trophic_shape <- function(reg, t_res=0.5){
 		y <- as.numeric(colnames(vv2))
 		z <- vv2
 
-		bulge <- c()
-		scale_sum1 <- function(x){
-			zero_min <- (x - min(x))
-			zero_min/sum(zero_min)
-		}
+		mid <- c()
+		# scale_sum1 <- function(x){
+	# 		zero_min <- (x - min(x))
+	# 		zero_min/sum(zero_min)
+	# 	}
 		for(i in 1:length(x)){
-			bulge[i] <- weighted.mean(y, w=scale_sum1(z[i,]))
+			# mid[i] <- weighted.mean(y, w=scale_sum1(z[i,]))
+			mid[i] <- t_mean(z[i,], y)
+			
+			if(i == 1){
+				sk <- t_skew(z[i,], y)
+				bul <- t_bulge(z[i,], y)
+			}else{
+				sk <- rbind(sk, t_skew(z[i,], y))
+				bul <- rbind(bul, t_bulge(z[i,], y))
+			}
 		}
 	
-		return(list(vv=vv, bulge=bulge, x=x, y=y, z=z))
+		return(list(vv=vv, mid=mid, sk=sk, bul=bul, x=x, y=y, z=z))
 	}
 
 	mass_l <- cast_tl(Xa2, "m")
 	# image.plot(x=mass_l$x, y=mass_l$y, z=mass_l$z, xlab="year", ylab="trophic level")
-	# lines(x=mass_l$x, y=mass_l$bulge, col="white", lwd=2)
+	# lines(x=mass_l$x, y=mass_l$mid, col="white", lwd=2)
 
 	rich_l <- cast_tl(Xa2, "r")
 	# image.plot(x=rich_l$x, y=rich_l$y, z=rich_l$z, xlab="year", ylab="trophic level")
-	# lines(x=rich_l$x, y=rich_l$bulge, col="white", lwd=2)
+	# lines(x=rich_l$x, y=rich_l$mid, col="white", lwd=2)
 	
 	return(list(mass_l=mass_l, rich_l=rich_l))
 	
 }
+
+
+getShape <- function(skew, bulge){
+	skew_0 <- skew[,"slope_p"] > 0.05*2
+	skew_p <- skew[,"slope"] > 0 & !skew_0
+	skew_n <- skew[,"slope"] < 0 & !skew_0
+	
+	bulge_0 <- bulge[,"slope_p"] > 0.05*2 & skew_0
+	bulge_p <- bulge[,"slope"] > 0 & !bulge_0 & skew_0
+	bulge_n <- bulge[,"slope"] < 0 & !bulge_0 & skew_0
+	
+	shape <- rep(NA, length(skew_0))
+	
+	shape[skew_p] <- "downward_triangle"
+	shape[skew_n] <- "upward_triangle"
+	shape[bulge_0] <- "rectangle"
+	shape[bulge_p] <- "diamond"
+	shape[bulge_n] <- "hourglass"
+	
+	return(shape)
+	
+}
+
+
